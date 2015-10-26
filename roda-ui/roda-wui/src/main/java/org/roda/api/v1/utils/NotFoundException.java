c6a85210adcd10bb58899ab5674757fcf730eb9b
Hélder Silva <hsilva@keep.pt>
2015-10-26 18:23:25 +0000

    misc: added plugin for adding/editing/removing license header to source code

--- roda-ui/roda-wui/src/main/java/org/roda/api/v1/utils/NotFoundException.java
+++ roda-ui/roda-wui/src/main/java/org/roda/api/v1/utils/NotFoundException.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE file at the root of the source
+ * tree and available online at
+ *
+ * https://github.com/keeps/roda
+ */
 package org.roda.api.v1.utils;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2015-09-03T11:38:49.275+01:00")
