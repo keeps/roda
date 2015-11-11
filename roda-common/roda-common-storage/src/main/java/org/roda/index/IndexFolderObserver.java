432e89037335ce46fb2419cd77ed5d5dc3bbe821
Sébastien Leroux <sleroux@keep.pt>
2015-11-11 15:05:01 +0000

    Updated IndexFolderObserver (paths)

--- roda-common/roda-common-storage/src/main/java/org/roda/index/IndexFolderObserver.java
+++ roda-common/roda-common-storage/src/main/java/org/roda/index/IndexFolderObserver.java
@@ -1,6 +1,7 @@
 package org.roda.index;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
 
@@ -27,22 +28,33 @@ public class IndexFolderObserver implements FolderObserver {
   public void pathAdded(Path basePath, Path createdPath) {
     LOGGER.debug("PATH CREATED: " + createdPath);
     try {
+      Path relativePath = basePath.relativize(createdPath);
+      
+      
       SolrInputDocument pathDocument = new SolrInputDocument();
-      String id = createdPath.toString();
-      pathDocument.addField(RodaConstants.SIPMONITOR_ID, id);
+      pathDocument.addField(RodaConstants.SIPMONITOR_ID, relativePath.toString());
       pathDocument.addField(RodaConstants.SIPMONITOR_FULLPATH, createdPath.toString());
       if (createdPath.getParent().compareTo(basePath) != 0) {
-        pathDocument.addField(RodaConstants.SIPMONITOR_PARENTPATH, createdPath.getParent().toString());
+        pathDocument.addField(RodaConstants.SIPMONITOR_PARENTPATH, relativePath.getParent().toString());
       }
-      pathDocument.addField(RodaConstants.SIPMONITOR_RELATIVEPATH, basePath.relativize(createdPath).toString());
+      pathDocument.addField(RodaConstants.SIPMONITOR_RELATIVEPATH, relativePath.toString());
       pathDocument.addField(RodaConstants.SIPMONITOR_DATE, new Date());
       if (createdPath.toFile().isDirectory()) {
         pathDocument.addField(RodaConstants.SIPMONITOR_ISFILE, false);
+        pathDocument.addField(RodaConstants.SIPMONITOR_SIZE, 0L);
       } else {
         pathDocument.addField(RodaConstants.SIPMONITOR_ISFILE, true);
+        long fileSize = Files.size(createdPath);
+        pathDocument.addField(RodaConstants.SIPMONITOR_SIZE, fileSize);
+        updateSize(fileSize,relativePath.getParent());
       }
-      pathDocument.addField(RodaConstants.SIPMONITOR_SIZE, 1000l);
-      pathDocument.addField(RodaConstants.SIPMONITOR_NAME, createdPath.getFileName().toString());
+      
+      pathDocument.addField(RodaConstants.SIPMONITOR_NAME, relativePath.getFileName().toString());
+      
+      LOGGER.debug("DIC");
+      for(String s : pathDocument.getFieldNames()){
+        LOGGER.debug(s+" - "+pathDocument.getFieldValue(s));
+      }
       index.add(RodaConstants.INDEX_SIP, pathDocument);
     } catch (IOException | SolrServerException e) {
       LOGGER.error("Error adding path to SIPMonitorIndex");
@@ -52,6 +64,11 @@ public class IndexFolderObserver implements FolderObserver {
     } catch (SolrServerException | IOException e) {
       LOGGER.error("Could not commit indexed path to SIPMonitor index", e);
     }
+  }
+
+  private void updateSize(long fileSize, Path p) {
+    // TODO UPDATE SIZE of path and call recursive...
+    
   }
 
   @Override
