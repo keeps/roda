3d72ca0893b747e3d4523820631a294e19f3039e
Luis Faria <lfaria@keep.pt>
2016-07-09 14:11:59 +0100

    Added cache mechanism for RODA URI File resolver, also fixing file leak, should close #398

--- roda-core/roda-core/src/main/java/org/roda/core/common/RodaURIFileResolver.java
+++ roda-core/roda-core/src/main/java/org/roda/core/common/RodaURIFileResolver.java
@@ -1,26 +1,49 @@
 package org.roda.core.common;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.URIResolver;
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.commons.compress.utils.IOUtils;
 import org.roda.core.RodaCoreFactory;
 import org.roda.core.data.common.RodaConstants;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 class RodaURIResolver implements URIResolver {
 
+  private static CacheLoader<String, ByteArrayInputStream> loader = new CacheLoader<String, ByteArrayInputStream>() {
+
+    @Override
+    public ByteArrayInputStream load(String href) throws Exception {
+      InputStream in = RodaCoreFactory
+        .getConfigurationFileAsStream(RodaConstants.CROSSWALKS_DISSEMINATION_OTHER_PATH + "/" + href);
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      IOUtils.copy(in, out);
+
+      return new ByteArrayInputStream(out.toByteArray());
+    }
+
+  };
+  private static LoadingCache<String, ByteArrayInputStream> cache = CacheBuilder.newBuilder()
+    .expireAfterWrite(1, TimeUnit.MINUTES).build(loader);
+
   @Override
-  public Source resolve(String href, String base) throws TransformerException {
+  public Source resolve(String href, String base) throws TransformerException {   
     try {
-      InputStream st = RodaCoreFactory
-      .getConfigurationFileAsStream(RodaConstants.CROSSWALKS_DISSEMINATION_OTHER_PATH + "/" + href);
-      return new StreamSource(st);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      return null;
+      ByteArrayInputStream in = cache.get(href);
+      return new StreamSource(in);
+    } catch (ExecutionException e) {
+      throw new TransformerException("Could not load URI: "+href, e);
     }
   }
 }
