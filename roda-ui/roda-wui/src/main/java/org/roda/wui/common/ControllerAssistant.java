5b8aa4fd91a79258cfd17dde08eca97c12704c2b
Rui Castro <rui.castro@gmail.com>
2016-07-22 01:26:51 +0100

    Added log registry in failed checkRoles

--- roda-ui/roda-wui/src/main/java/org/roda/wui/common/ControllerAssistant.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/common/ControllerAssistant.java
@@ -6,37 +6,37 @@ import org.roda.core.common.UserUtility;
 import org.roda.core.data.exceptions.AuthorizationDeniedException;
 import org.roda.core.data.v2.log.LogEntry.LOG_ENTRY_STATE;
 import org.roda.core.data.v2.user.RodaUser;
+import java.lang.reflect.Method;
 
 public class ControllerAssistant {
-  private Date startDate;
+  private final Date startDate;
+  private final Method enclosingMethod;
 
   public ControllerAssistant() {
     this.startDate = new Date();
+    this.enclosingMethod = this.getClass().getEnclosingMethod();
   }
 
   public void checkRoles(RodaUser user) throws AuthorizationDeniedException {
-    UserUtility.checkRoles(user, this.getClass());
+    try {
+      UserUtility.checkRoles(user, this.getClass());
+    } catch (AuthorizationDeniedException e) {
+      registerAction(user, LOG_ENTRY_STATE.UNAUTHORIZED);
+      throw e;
+    }
   }
 
   public void registerAction(RodaUser user, String aipId, LOG_ENTRY_STATE state, Object... parameters) {
     long duration = new Date().getTime() - startDate.getTime();
-    RodaCoreService.registerAction(user, getInvokingClassName(), getInvokingMethodName(), aipId, duration, state,
-      parameters);
+    RodaCoreService.registerAction(user, this.enclosingMethod.getDeclaringClass().getName(),
+      this.enclosingMethod.getName(), aipId, duration, state, parameters);
   }
 
   public void registerAction(RodaUser user, LOG_ENTRY_STATE state, Object... parameters) {
-    registerAction(user, (String) null, state, parameters);
+    registerAction(user, null, state, parameters);
   }
 
   public void registerAction(RodaUser user, LOG_ENTRY_STATE state) {
     registerAction(user, (String) null, state);
-  }
-
-  private String getInvokingMethodName() {
-    return this.getClass().getEnclosingMethod().getName();
-  }
-
-  private String getInvokingClassName() {
-    return this.getClass().getEnclosingMethod().getDeclaringClass().getName();
   }
 }
