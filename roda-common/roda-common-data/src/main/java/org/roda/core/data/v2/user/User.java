8e77a3123a8b8fdf1b37bcd346e6bf41614e55b1
Rui Castro <rui.castro@gmail.com>
2016-08-10 16:08:08 +0100

    Removed old attributes from User

--- roda-common/roda-common-data/src/main/java/org/roda/core/data/v2/user/User.java
+++ roda-common/roda-common-data/src/main/java/org/roda/core/data/v2/user/User.java
@@ -7,9 +7,6 @@
  */
 package org.roda.core.data.v2.user;
 
-import java.util.Arrays;
-import java.util.Date;
-
 /**
  * This is a user of RODA.
  * 
@@ -26,49 +23,16 @@ public class User extends RodaUser {
   // FIXME change this to list?
   public static final String[] ID_TYPES = new String[] {ID_TYPE_BI, ID_TYPE_PASSPORT, ID_TYPE_CITIZEN_CARD};
 
-  // LDAP documentTitle
-  private String idDocumentType = null;
-  // LDAP documentIdentifier
-  private String idDocument = null;
-  // LDAP documentLocation
-  private String idDocumentLocation = null;
-  // LDAP documentVersion
-  private Date idDocumentDate = null;
-
-  // LDAP serialNumber
-  private String financeIdentificationNumber = null;
-
-  // LDAP friendlyCountryName
-  private String birthCountry = null;
-
-  // LDAP postalAddress
-  private String postalAddress = null;
-  // LDAP postalCode
-  private String postalCode = null;
-  // LDAP l
-  private String localityName = null;
-  // LDAP c
-  private String countryName = null;
-  // LDAP telephoneNumber
-  private String telephoneNumber = null;
-  // LDAP fax
-  private String fax = null;
-  // // LDAP email
-  // private String email = null;
-
-  // LDAP businessCategory
-  private String businessCategory = null;
-
-  // LDAP info
+  /** LDAP info */
   private String resetPasswordToken = null;
-  // LDAP info
+  /** LDAP info */
   private String resetPasswordTokenExpirationDate = null;
-  // LDAP info
+  /** LDAP info */
   private String emailConfirmationToken = null;
-  // LDAP info
+  /** LDAP info */
   private String emailConfirmationTokenExpirationDate = null;
 
-  /** Extra information about this user. */
+  /** LDAP description */
   private String extra = null;
 
   /**
@@ -98,34 +62,23 @@ public class User extends RodaUser {
    *          the User to be cloned.
    */
   public User(final User user) {
-
     super(user.getId(), user.getName(), user.getEmail(), false, user.getIpAddress(), user.getDirectGroups(),
       user.getAllGroups(), user.getDirectRoles(), user.getAllRoles());
     setActive(user.isActive());
-    setIdDocumentType(user.getIdDocumentType());
-    setIdDocument(user.getIdDocument());
-    setIdDocumentLocation(user.getIdDocumentLocation());
-    setIdDocumentDate(user.getIdDocumentDate());
-
-    setFinanceIdentificationNumber(user.getFinanceIdentificationNumber());
-    setBirthCountry(user.getBirthCountry());
-
-    setPostalAddress(user.getPostalAddress());
-    setPostalCode(user.getPostalCode());
-    setLocalityName(user.getLocalityName());
-    setCountryName(user.getCountryName());
-    setTelephoneNumber(user.getTelephoneNumber());
-    setFax(user.getFax());
     setEmail(user.getEmail());
-
-    setBusinessCategory(user.getBusinessCategory());
-
+    setExtra(user.getExtra());
     setResetPasswordToken(user.getResetPasswordToken());
     setResetPasswordTokenExpirationDate(user.getResetPasswordTokenExpirationDate());
     setEmailConfirmationToken(user.getResetPasswordToken());
     setEmailConfirmationTokenExpirationDate(user.getResetPasswordTokenExpirationDate());
   }
 
+  /**
+   * Construct a new User cloning a given {@link RodaUser}
+   * 
+   * @param rodaUser
+   *          the {@link RodaUser} to be cloned.
+   */
   public User(final RodaUser rodaUser) {
     super(rodaUser);
   }
@@ -134,112 +87,10 @@ public class User extends RodaUser {
    * @see RODAMember#toString()
    */
   public String toString() {
-    return "User (" + super.toString() + ", idDocumentType=" + getIdDocumentType() + ", idDocument=" + getIdDocument()
-      + ", idDocumentLocation=" + getIdDocumentLocation() + ", idDocumentDate=" + getIdDocumentDate()
-      + ", financeIdentificationNumber=" + getFinanceIdentificationNumber() + ", birthCountry=" + getBirthCountry()
-      + ", postalAddress=" + getPostalAddress() + ", postalCode=" + getPostalCode() + ", localityName="
-      + getLocalityName() + ", countryName=" + getCountryName() + ", telephoneName=" + getTelephoneNumber() + ", fax="
-      + getFax() + ", email=" + getEmail() + ", businessCategory=" + getBusinessCategory() + ", extra=" + getExtra()
-      + ", resetPasswordToken=" + getResetPasswordToken() + ", resetPasswordTokenExpirationDate="
-      + getResetPasswordTokenExpirationDate() + ", emailConfirmationToken=" + getEmailConfirmationToken()
-      + ", emailConfirmationTokenExpirationDate=" + getEmailConfirmationTokenExpirationDate() + ")";
-  }
-
-  /**
-   * @return the idDocumentType
-   */
-  public String getIdDocumentType() {
-    return idDocumentType;
-  }
-
-  /**
-   * @param idDocumentType
-   *          the idDocumentType to set
-   */
-  public void setIdDocumentType(String idDocumentType) {
-
-    if (idDocumentType == null || Arrays.asList(ID_TYPES).contains(idDocumentType.toLowerCase())) {
-
-      this.idDocumentType = idDocumentType;
-
-    } else {
-      throw new IllegalArgumentException("'" + idDocumentType + "' is not a valid ID document type");
-    }
-  }
-
-  /**
-   * @return the idDocument
-   */
-  public String getIdDocument() {
-    return idDocument;
-  }
-
-  /**
-   * @param idDocument
-   *          the idDocument to set
-   */
-  public void setIdDocument(String idDocument) {
-    this.idDocument = idDocument;
-  }
-
-  /**
-   * @return the idDocumentLocation
-   */
-  public String getIdDocumentLocation() {
-    return idDocumentLocation;
-  }
-
-  /**
-   * @param idDocumentLocation
-   *          the idDocumentLocation to set
-   */
-  public void setIdDocumentLocation(String idDocumentLocation) {
-    this.idDocumentLocation = idDocumentLocation;
-  }
-
-  /**
-   * @return the idDocumentDate
-   */
-  public Date getIdDocumentDate() {
-    return idDocumentDate;
-  }
-
-  /**
-   * @param idDocumentDate
-   *          the idDocumentDate to set
-   */
-  public void setIdDocumentDate(Date idDocumentDate) {
-    this.idDocumentDate = idDocumentDate;
-  }
-
-  /**
-   * @return the financeIdentificationNumber
-   */
-  public String getFinanceIdentificationNumber() {
-    return financeIdentificationNumber;
-  }
-
-  /**
-   * @param financeIdentificationNumber
-   *          the financeIdentificationNumber to set
-   */
-  public void setFinanceIdentificationNumber(String financeIdentificationNumber) {
-    this.financeIdentificationNumber = financeIdentificationNumber;
-  }
-
-  /**
-   * @return the birthCountry
-   */
-  public String getBirthCountry() {
-    return birthCountry;
-  }
-
-  /**
-   * @param birthCountry
-   *          the birthCountry to set
-   */
-  public void setBirthCountry(String birthCountry) {
-    this.birthCountry = birthCountry;
+    return "User (" + super.toString() + ", email=" + getEmail() + ", extra=" + getExtra() + ", resetPasswordToken="
+      + getResetPasswordToken() + ", resetPasswordTokenExpirationDate=" + getResetPasswordTokenExpirationDate()
+      + ", emailConfirmationToken=" + getEmailConfirmationToken() + ", emailConfirmationTokenExpirationDate="
+      + getEmailConfirmationTokenExpirationDate() + ")";
   }
 
   /**
@@ -258,111 +109,22 @@ public class User extends RodaUser {
   }
 
   /**
-   * 
-   * @return the telephone number
+   * Get User's extra information.
+   *
+   * @return a {@link String} with user's extra information.
    */
-  public String getTelephoneNumber() {
-    return telephoneNumber;
+  public String getExtra() {
+    return extra;
   }
 
   /**
-   * 
-   * @param telephoneNumber
-   *          telephone number
+   * Set {@link User}'s extra information.
+   *
+   * @param extra
+   *          a {@link String} with user's extra information.
    */
-  public void setTelephoneNumber(String telephoneNumber) {
-    this.telephoneNumber = telephoneNumber;
-  }
-
-  /**
-   * 
-   * @return the postal address
-   */
-  public String getPostalAddress() {
-    return postalAddress;
-  }
-
-  /**
-   * @param postalAddress
-   *          the postal address
-   */
-  public void setPostalAddress(String postalAddress) {
-    this.postalAddress = postalAddress;
-  }
-
-  /**
-   * @return the postal code
-   */
-  public String getPostalCode() {
-    return postalCode;
-  }
-
-  /**
-   * @param postalCode
-   *          the postal code to set
-   */
-  public void setPostalCode(String postalCode) {
-    this.postalCode = postalCode;
-  }
-
-  /**
-   * @return the locality name
-   */
-  public String getLocalityName() {
-    return localityName;
-  }
-
-  /**
-   * @param localityName
-   *          the locality name to set
-   */
-  public void setLocalityName(String localityName) {
-    this.localityName = localityName;
-  }
-
-  /**
-   * @return the country name
-   */
-  public String getCountryName() {
-    return countryName;
-  }
-
-  /**
-   * @param countryName
-   *          the country name to set
-   */
-  public void setCountryName(String countryName) {
-    this.countryName = countryName;
-  }
-
-  /**
-   * @return the fax
-   */
-  public String getFax() {
-    return fax;
-  }
-
-  /**
-   * @param fax
-   *          the fax to set
-   */
-  public void setFax(String fax) {
-    this.fax = fax;
-  }
-
-  /**
-   * @return the businessCategory
-   */
-  public String getBusinessCategory() {
-    return businessCategory;
-  }
-
-  /**
-   * @param businessCategory
-   *          the businessCategory to set
-   */
-  public void setBusinessCategory(String businessCategory) {
-    this.businessCategory = businessCategory;
+  public void setExtra(final String extra) {
+    this.extra = extra;
   }
 
   /**
@@ -423,25 +185,6 @@ public class User extends RodaUser {
    */
   public void setEmailConfirmationTokenExpirationDate(final String emailConfirmationTokenExpirationDate) {
     this.emailConfirmationTokenExpirationDate = emailConfirmationTokenExpirationDate;
-  }
-
-  /**
-   * Get User's extra information.
-   * 
-   * @return a {@link String} with user's extra information.
-   */
-  public String getExtra() {
-    return extra;
-  }
-
-  /**
-   * Set {@link User}'s extra information.
-   * 
-   * @param extra
-   *          a {@link String} with user's extra information.
-   */
-  public void setExtra(final String extra) {
-    this.extra = extra;
   }
 
 }
