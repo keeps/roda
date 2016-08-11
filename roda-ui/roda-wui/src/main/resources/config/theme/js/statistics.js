f5c73cf0b56148af4d2c04560af0652ffa9f6b64
Rui Castro <rui.castro@gmail.com>
2016-08-11 23:42:15 +0100

    WIP - Added sample chart to Statistics.html

---
+++ roda-ui/roda-wui/src/main/resources/config/theme/js/statistics.js
@@ -0,0 +1,69 @@
+$(function(){
+    // select the target node
+    var target = document.body;
+
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            console.log(mutation.type);
+            initChart();
+        });
+    });
+
+    // configuration of the observer:
+    var config = { attributes: true, childList: true, characterData: true };
+
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config);
+
+    // later, you can stop observing
+    // observer.disconnect();
+
+    var initChart = function(){
+        console.log("initChart()");
+        $("#statistics #canvasChart").each(function(){
+            console.log("initChart(): each this=", this);
+            if (this.chart == null) {
+                this.chart = new Chart(this, {
+                    type: 'bar',
+                    data: {
+                        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+            } else {
+                // this.chart != null
+                console.log("this.chart already exists");
+            }
+        });
+    }
+});
