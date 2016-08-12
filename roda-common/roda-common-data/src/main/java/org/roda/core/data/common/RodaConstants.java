27bdefcd90577a23d6939966f60aa1a0e1088646
Rui Castro <rui.castro@gmail.com>
2016-08-12 16:16:40 +0100

    Added suport for factets in REST API index/find

--- roda-common/roda-common-data/src/main/java/org/roda/core/data/common/RodaConstants.java
+++ roda-common/roda-common-data/src/main/java/org/roda/core/data/common/RodaConstants.java
@@ -140,8 +140,9 @@ public final class RodaConstants {
   public static final String API_QUERY_VALUE_LANG_EN_US = "en_US";
   public static final String API_QUERY_KEY_START = "start";
   public static final String API_QUERY_KEY_LIMIT = "limit";
-  public static final String API_QUERY_RETURN_CLASS = "returnClass";
-  public static final String API_QUERY_ONLY_ACTIVE = "onlyActive";
+  public static final String API_QUERY_KEY_RETURN_CLASS = "returnClass";
+  public static final String API_QUERY_KEY_ONLY_ACTIVE = "onlyActive";
+  public static final String API_QUERY_KEY_FACET = "facet";
   // "http path param" related strings
   public static final String API_PATH_PARAM_AIP_ID = "aip_id";
   public static final String API_PATH_PARAM_REPRESENTATION_ID = "representation_id";
