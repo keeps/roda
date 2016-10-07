9a43f9a1591d36a138698ddb356a73c87430c901
Rui Castro <rui.castro@gmail.com>
2016-10-07 17:53:24 +0100

    Updated and enabled CAS auth filter Minor improvements to internal auth filter

--- roda-ui/roda-wui/src/main/java/org/roda/wui/filter/RodaCasAuthenticationFilter.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/filter/RodaCasAuthenticationFilter.java
@@ -2,7 +2,7 @@
  * The contents of this file are subject to the license and copyright
  * detailed in the LICENSE file at the root of the source
  * tree and available online at
- *
+ * <p>
  * https://github.com/keeps/roda
  */
 package org.roda.wui.filter;
@@ -22,38 +22,23 @@ import javax.servlet.http.HttpServletResponse;
 import org.jasig.cas.client.util.CommonUtils;
 import org.roda.core.RodaCoreFactory;
 import org.roda.core.common.UserUtility;
-import org.roda.core.data.common.RodaConstants;
+import org.roda.core.data.exceptions.GenericException;
 import org.roda.core.data.exceptions.RODAException;
-import org.roda.core.data.v2.index.filter.SimpleFilterParameter;
-import org.roda.core.data.v2.user.RODAMember;
 import org.roda.core.data.v2.user.User;
+import org.roda.wui.client.common.utils.StringUtils;
+import org.roda.wui.client.welcome.Welcome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Servlet Filter implementation class RolesSetterFilter
+ * CAS authentication filter.
  */
 public class RodaCasAuthenticationFilter implements Filter {
-  private static final Logger logger = LoggerFactory.getLogger(RodaCasAuthenticationFilter.class);
+  /** Logger. */
+  private static final Logger LOGGER = LoggerFactory.getLogger(RodaCasAuthenticationFilter.class);
 
-  private FilterConfig config;
-  protected String casLogoutURL = null;
-
-  public void setFilterConfig(FilterConfig config) {
-    this.config = config;
-  }
-
-  public FilterConfig getFilterConfig() {
-    return config;
-  }
-
-  public void init(FilterConfig config) throws ServletException {
-    setFilterConfig(config);
-
-    casLogoutURL = RodaCoreFactory.getRodaConfiguration().getString("roda.cas.url") + "/logout";
-
-    logger.info(getClass().getSimpleName() + " initialized ok");
-  }
+  /** URL to logout in CAS service. */
+  private String casLogoutURL;
 
   /**
    * Default constructor.
@@ -62,83 +47,118 @@ public class RodaCasAuthenticationFilter implements Filter {
     // do nothing
   }
 
+  @Override
+  public void init(final FilterConfig config) throws ServletException {
+    casLogoutURL = String.format("%s/logout", config.getInitParameter("casServerUrlPrefix"));
+
+    LOGGER.info(getClass().getSimpleName() + " initialized ok");
+  }
+
   /**
    * @see Filter#destroy()
    */
+  @Override
   public void destroy() {
     // do nothing
   }
 
   /**
+   * @param request
+   *          the request.
+   * @param response
+   *          the response.
+   * @param chain
+   *          the filter chain.
+   * @throws IOException
+   *           if some I/O error occurs.
+   * @throws ServletException
+   *           if some error occurs.
    * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
    */
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+  @Override
+  public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
     throws IOException, ServletException {
-    try {
-      HttpServletRequest servletRequest = (HttpServletRequest) request;
-      HttpServletResponse servletResponse = (HttpServletResponse) response;
-      if (servletRequest.getUserPrincipal() != null) {
-        if (userExists(servletRequest.getUserPrincipal().getName())) {
-          logger.debug("User principal and user exist (" + servletRequest.getUserPrincipal().getName() + ")");
-          UserUtility.setUser(servletRequest, getUser(servletRequest.getUserPrincipal()));
-        } else {
-          logger.debug("User principal exist but user doesn't (" + servletRequest.getUserPrincipal().getName() + ")");
-          User rsu = getUser(servletRequest.getUserPrincipal());
-          logger.debug("Adding user to ldap/index: " + rsu);
-          addUserToLdapAndIndex(request, rsu);
-          UserUtility.setUser(servletRequest, rsu);
-        }
-      } else {
-        UserUtility.setUser(servletRequest, UserUtility.getUser(servletRequest));
+
+    final HttpServletRequest httpRequest = (HttpServletRequest) request;
+    final HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+    final String url = httpRequest.getRequestURL().toString();
+    final String requestURI = httpRequest.getRequestURI();
+    final String service = httpRequest.getParameter("service");
+    final String hash = httpRequest.getParameter("hash");
+    final String locale = httpRequest.getParameter("locale");
+
+    LOGGER.debug("URL: {} ; Request URI: {} ; Service: {} ; Hash: {}, Locale: {}", url, requestURI, service, hash,
+      locale);
+
+    final Principal principal = httpRequest.getUserPrincipal();
+    if (principal == null) {
+      UserUtility.setUser(httpRequest, UserUtility.getUser(httpRequest));
+    } else {
+      UserUtility.setUser(httpRequest, getOrCreateUser(principal.getName()));
+    }
+
+    if (url.endsWith("/login")) {
+
+      final StringBuilder b = new StringBuilder();
+      b.append("/");
+
+      if (StringUtils.isNotBlank(locale)) {
+        b.append("?locale=").append(locale);
       }
 
-      String url = servletRequest.getRequestURL().toString();
-      if (url.endsWith("/login")) {
-        url = url.substring(0, url.indexOf("login"));
-        servletResponse.sendRedirect(url);
-      } else if (url.endsWith("/logout")) {
-        url = url.substring(0, url.indexOf("logout"));
-        UserUtility.logout(servletRequest);
-        String urlToRedirectTo = CommonUtils.constructRedirectUrl(casLogoutURL, "service", url, false, false);
-        servletResponse.sendRedirect(urlToRedirectTo);
-      } else {
-        chain.doFilter(request, response);
+      if (StringUtils.isNotBlank(hash)) {
+        b.append("#").append(hash);
       }
 
-    } catch (RuntimeException t) {
-      logger.error(t.getMessage(), t);
+      httpResponse.sendRedirect(b.toString());
+
+    } else if (url.endsWith("/logout")) {
+
+      UserUtility.logout(httpRequest);
+
+      final StringBuilder b = new StringBuilder();
+      b.append(url.substring(0, url.indexOf("logout")));
+
+      if (StringUtils.isNotBlank(locale)) {
+        b.append("?locale=").append(locale);
+      }
+
+      b.append("#").append(Welcome.RESOLVER.getHistoryToken());
+
+      httpResponse.sendRedirect(CommonUtils.constructRedirectUrl(casLogoutURL, "service", b.toString(), false, false));
+
+    } else {
+      chain.doFilter(request, response);
     }
+
   }
 
-  // TODO test this
-  private void addUserToLdapAndIndex(ServletRequest request, User userPrincipal) {
+  private User getOrCreateUser(final String name) {
+    User user;
     try {
-      final User user = new User(userPrincipal);
-      RodaCoreFactory.getModelService().createUser(user, true);
-    } catch (RODAException e) {
-      logger.error("Error while creating and indexing user", e);
+
+      user = RodaCoreFactory.getModelService().retrieveUserByName(name);
+      LOGGER.debug("User principal and user exist (" + name + ")");
+
+    } catch (final GenericException e) {
+      LOGGER.debug("Error getting user '" + name + "' - " + e.getMessage(), e);
+      LOGGER.debug("User principal exist but user doesn't (" + name + ")");
+
+      user = new User(name);
+      LOGGER.debug("Adding user to ldap/index: " + user);
+      user = createUser(user);
     }
+    return user;
   }
 
-  private User getUser(Principal userPrincipal) {
-    User rsu = new User();
-    rsu.setId(userPrincipal.getName());
-    rsu.setGuest(false);
-    return rsu;
-  }
-
-  private boolean userExists(String name) {
-    boolean exist;
-    org.roda.core.data.v2.index.filter.Filter filter = new org.roda.core.data.v2.index.filter.Filter();
-    filter.add(new SimpleFilterParameter(RodaConstants.MEMBERS_ID, name));
-    filter.add(new SimpleFilterParameter(RodaConstants.MEMBERS_IS_USER, "true"));
+  private User createUser(final User user) {
     try {
-      Long count = RodaCoreFactory.getIndexService().count(RODAMember.class, filter);
-      exist = (count == 1);
-    } catch (RODAException e) {
-      exist = false;
+      return RodaCoreFactory.getModelService().createUser(user, true);
+    } catch (final RODAException e) {
+      LOGGER.error("Error while creating and indexing user - " + e.getMessage(), e);
+      return user;
     }
-
-    return exist;
   }
+
 }
