c5fc8ac5aa29fbf386dc2b14c009642070d119a2
Nuno Vieira <nvieira@keep.pt>
2016-10-11 10:15:40 +0100

    theme get version issue solved

--- roda-ui/roda-wui/src/main/resources/config/theme/theme.js
+++ roda-ui/roda-wui/src/main/resources/config/theme/theme.js
@@ -1,36 +1,45 @@
+var FOOTER_ADDED = false;
+var SIDEBAR_ADDED = false;
 
 $(document).ready(function() {
+
+	$(document).on('DOMNodeInserted', ".footer", function(e) {
+		elem = e.target;
+		if(!FOOTER_ADDED && $(elem).hasClass("footer")) {
+			$.get("/version.json", function( data ) {
+			      $(".footer").append("<div style='color:#999; float:right;' class='build_time'>Version build on ola</div>");
+			      FOOTER_ADDED = true;
+			});
+		}
+	});
 	
 	$(document).on('DOMNodeInserted', ".sidebar_actions_wrapper", function(e) {
-		if($(this).hasClass("sidebar_actions_wrapper")) {
+		elem = e.target;
+		if(!SIDEBAR_ADDED && $(elem).hasClass("sidebar_actions_wrapper")) {
 			setFacetsWidth();
+			SIDEBAR_ADDED = true;
 		}
 	});
 
-	
-	$(document).on('DOMNodeInserted', ".footer", function(e) {
-		$.get("/version.json", function( data ) {
-		      $(".footer").append("<div style='color:#999; float:right;' class='build_time'>Version build on " + data["git.build.time"] + "</div>");
-		});
-	});
-
-	
 	$(window).resize(function() {
 		setFacetsWidth();
 	});
 	
 	
 	$(window).scroll(function () {
-	if ($(window).scrollTop() + $(".sidebar_actions_wrapper").height() < $(".sidebar").position().top + $(".sidebar").height() ) {
-      $('.sidebar_actions_wrapper').addClass('sticky');
-  	  setFacetsWidth();
-    } else {
-      $('.sidebar_actions_wrapper').removeClass('sticky');
-    }
-  });
+		if ($(".sidebar").length > 0 && $(window).height() > $(".sidebar_actions_wrapper").height() &&
+				$(window).scrollTop() + $(".sidebar_actions_wrapper").height() < $(".sidebar").position().top + $(".sidebar").height()) {
+	      $('.sidebar_actions_wrapper').addClass('sticky');
+	  	  setFacetsWidth();
+	    } else {
+	      $('.sidebar_actions_wrapper').removeClass('sticky');
+	    }
+   	});
 });
 
 
 function setFacetsWidth() {
-	$('.sidebar_actions_wrapper').css('width', $(".sidebar").width());
+	if($('.sidebar_actions_wrapper').length > 0) {
+		$('.sidebar_actions_wrapper').css('width', $(".sidebar").width());
+	}
 }
