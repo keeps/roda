3b27d19ef62b01614a273c0bb8da1427dbb1e768
Luis Faria <lfaria@keep.pt>
2016-10-11 14:37:21 +0100

    fixing version adding in theme

--- roda-ui/roda-wui/src/main/resources/config/theme/theme.js
+++ roda-ui/roda-wui/src/main/resources/config/theme/theme.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
 		elem = e.target;
 		if(!FOOTER_ADDED && $(elem).hasClass("footer")) {
 			$.get("/version.json", function( data ) {
-			      $(".footer").append("<div style='color:#999; float:right;' class='build_time'>Version build on ola</div>");
+			      $(".footer").append("<div style='color:#999; float:right;' class='build_time'>Version build on " + data["git.build.time"] + "</div>");
 			      FOOTER_ADDED = true;
 			});
 		}
 	});
-	
+
 	$(document).on('DOMNodeInserted', ".sidebar_actions_wrapper", function(e) {
 		elem = e.target;
 		if(!SIDEBAR_ADDED && $(elem).hasClass("sidebar_actions_wrapper")) {
@@ -24,8 +24,8 @@ $(document).ready(function() {
 	$(window).resize(function() {
 		setFacetsWidth();
 	});
-	
-	
+
+
 	$(window).scroll(function () {
 		if ($(".sidebar").length > 0 && $(window).height() > $(".sidebar_actions_wrapper").height() &&
 				$(window).scrollTop() + $(".sidebar_actions_wrapper").height() < $(".sidebar").position().top + $(".sidebar").height()) {
