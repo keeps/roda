87eb460a470e100f89a6e06c98d9398eced554af
Sébastien Leroux <sleroux@keep.pt>
2016-10-12 10:03:20 +0100

    Fixed EARK Sip to AIP plugin (multiple ingested sip ids)

--- roda-core/roda-core/src/main/java/org/roda/core/plugins/plugins/ingest/EARKSIPToAIPPlugin.java
+++ roda-core/roda-core/src/main/java/org/roda/core/plugins/plugins/ingest/EARKSIPToAIPPlugin.java
@@ -127,6 +127,7 @@ public class EARKSIPToAIPPlugin extends SIPToAIPPlugin {
     SIP sip = null;
     try {
       sip = EARKSIP.parse(earkSIPPath, RodaCoreFactory.getWorkingDirectory());
+     
       reportItem.setSourceObjectOriginalId(sip.getId());
 
       if (sip.getValidationReport().isValid()) {
@@ -161,7 +162,7 @@ public class EARKSIPToAIPPlugin extends SIPToAIPPlugin {
                 Permissions.PermissionType.GRANT)));
             // Create the permissions object for the user that created the job
             aip = EARKSIPToAIPPluginUtils.earkSIPToAIP(sip, jobUsername, fullPermissions, model, storage,
-              Arrays.asList(sip.getId()), reportItem.getJobId(), computedParentId);
+              sip.getIds(), reportItem.getJobId(), computedParentId);
           } else {
             throw new GenericException("Unknown IP Status: " + sip.getStatus());
           }
