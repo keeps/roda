119886e68bf4409bfe129979df598b4936a858d1
Luis Faria <lfaria@keep.pt>
2016-11-07 15:09:56 +0000

    fixing mimetype wildcard

--- roda-common/roda-common-data/src/main/java/org/roda/core/data/common/RodaConstants.java
+++ roda-common/roda-common-data/src/main/java/org/roda/core/data/common/RodaConstants.java
@@ -1036,7 +1036,7 @@ public final class RodaConstants {
 
   public static final String INDEX_SEARCH_SUFFIX = "_txt";
 
-  public static final String MEDIA_TYPE_WILDCARD = "*";
+  public static final String MEDIA_TYPE_WILDCARD = "*/*";
 
   public final static String MEDIA_TYPE_APPLICATION_XML = "application/xml";
 
