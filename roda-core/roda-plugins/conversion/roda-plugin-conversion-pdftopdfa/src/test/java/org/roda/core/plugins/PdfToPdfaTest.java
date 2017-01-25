abf49e41b6e1e9d32412b0c1f16331b6aba29d07
Hélder Silva <hsilva@keep.pt>
2017-01-25 13:00:38 +0000

    core: fixed verapdf related issues (by not using gui but instead core & validation-model)

--- roda-core/roda-plugins/conversion/roda-plugin-conversion-pdftopdfa/src/test/java/org/roda/core/plugins/PdfToPdfaTest.java
+++ roda-core/roda-plugins/conversion/roda-plugin-conversion-pdftopdfa/src/test/java/org/roda/core/plugins/PdfToPdfaTest.java
@@ -158,7 +158,7 @@ public class PdfToPdfaTest {
     return model.retrieveAIP(indexedAIP.getId());
   }
 
-  @Test(enabled = false)
+  @Test
   public void testPdfToPdfaPlugin() throws RODAException, FileAlreadyExistsException, InterruptedException, IOException,
     SolrServerException, IsStillUpdatingException {
     AIP aip = ingestCorpora();
