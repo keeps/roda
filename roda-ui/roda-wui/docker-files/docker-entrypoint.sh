3778493aa6c8cc46ae0be26dc7677b916b2a6ebb
Luis Faria <lfaria@keep.pt>
2017-10-12 12:00:17 +0100

    adding gosu, reverting to priviledge de-escalation in script, adding cron back

--- roda-ui/roda-wui/docker-files/docker-entrypoint.sh
+++ roda-ui/roda-wui/docker-files/docker-entrypoint.sh
@@ -3,16 +3,17 @@
 # Either use the LOCAL_USER_ID and LOCAL_GROUP_ID
 # if passed in at runtime or fallback
 
-#USER_ID=${LOCAL_USER_ID:-9001}
-#GROUP_ID=${LOCAL_GROUP_ID:-9001}
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001}
 
 # Adding roda user and group
-#echo "Starting with UID : $USER_ID and GUID : $GROUP_ID"
-#groupadd -g $GROUP_ID roda
-#useradd  --shell /bin/bash --no-log-init -u $USER_ID -o -c "RODA" -g roda roda
+echo "Starting with UID : $USER_ID and GUID : $GROUP_ID"
+groupadd -g $GROUP_ID roda
+useradd  --shell /bin/bash --no-log-init -u $USER_ID -o -c "RODA" -g roda roda
 
-#su roda -c "export PATH=\"$PATH\""
-#su roda -c "export CATALINA_HOME=\"$CATALINA_HOME\""
+# Fixing permissions
+chown -R roda:roda /usr/local/tomcat
+mkdir -p /roda && chown roda:roda /roda
 
 # run extension scripts
 DIR=/docker-entrypoint.d
@@ -22,4 +23,9 @@ then
   /bin/run-parts --regex '^.*$' --verbose "$DIR"
 fi
 
-exec "$@"
+if [[ $# -eq 0 ]] ; then
+    echo 'Starting Apache Tomcat (user: roda)'
+    exec gosu roda catalina.sh run
+fi
+
+exec gosu roda $@
