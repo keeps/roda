ab959e45b1df3c23d85f0d2ec05460ed8dcf462b
Luis Faria <lfaria@keep.pt>
2018-04-24 11:09:04 +0100

    trying to enable clamav database cache

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,8 +2,5 @@
 
 set -ex
 
-# Enable Clamav database cache 
-chmod -R a+r "$CLAMAV_DATABASE"
-
 # code coverage
 # bash <(curl -s https://codecov.io/bash)
--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -17,6 +17,7 @@ sudo chmod a+rx /usr/bin/clamscan
 sudo mkdir -p "$CLAMAV_DATABASE"
 sudo chown clamav:clamav "$CLAMAV_DATABASE"
 sudo freshclam --datadir=$CLAMAV_DATABASE
+sudo -u clamav chmod -R a+r "$CLAMAV_DATABASE"
 
 # decrypt maven setting.xml
 openssl aes-256-cbc -K $encrypted_a8a9ca6bf122_key -iv $encrypted_a8a9ca6bf122_iv -in .travis/settings.xml.enc -out settings.xml -d
