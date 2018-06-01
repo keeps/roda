b04f2cc062ea952769b309fb2d1105540cf46914
Bruno Ferreira <chalkos@chalkos.net>
2018-06-01 11:18:25 +0100

    fixes #1157 by supporting a css filename to be passed in the 'branding' parameter. The css extension may be omitted as it is added automatically. Multiple brading CSS can be used by providing their names separated by commas in the 'branding' parameter, the CSS files are loaded in the order they are specified and after all the base CSS has been loaded. Using 'branding=false' is an alias for 'branding=nobranding.css' in order to keep retrocompatibility.

--- roda-ui/roda-wui/src/main/resources/config/theme/theme.js
+++ roda-ui/roda-wui/src/main/resources/config/theme/theme.js
@@ -1,14 +1,48 @@
 var FOOTER_ADDED = false;
 
 $(document).ready(function() {
+    // getUrlParameters function based on
+    // from https://stackoverflow.com/a/2880929/1483200
+    var urlParams = (function () {
+        var urlParams,
+            match,
+            pl = /\+/g,  // Regex for replacing addition symbol with a space
+            search = /([^&=]+)=?([^&]*)/g,
+            decode = function (s) {
+                return decodeURIComponent(s.replace(pl, " "));
+            },
+            query = window.location.search.substring(1);
 
-	var search = window.location.search;
-	if(/branding=false/.test(search)) {
-		$("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=nobranding.css">');
-	}
+        urlParams = {};
+        while (match = search.exec(query))
+            urlParams[decode(match[1])] = decode(match[2]);
+        return urlParams;
+    })();
+
+    // keep branding=false retrocompatibility
+    if (urlParams['branding'] === 'false') {
+        urlParams['branding'] = 'nobranding.css';
+    }
+
+    if ((typeof urlParams['branding']) === 'string') {
+        var brandings = urlParams['branding'].split(',');
+
+        brandings.forEach(function(branding){
+            if (branding.endsWith("css")) {
+                $(document).on('DOMNodeInserted', ".footer", function(e) {
+                    $("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=' + branding + '">');
+                });
+            } else {
+                // allow branding to be specified without extension
+                $(document).on('DOMNodeInserted', ".footer", function(e) {
+                    $("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=' + branding + '.css">');
+                });
+            }
+        });
+    }
 
 	// necessary to pass on the accessibility test
-	$(document).on('DOMNodeInserted', "thead", function(e) {		
+	$(document).on('DOMNodeInserted', "thead", function(e) {
 		$("img").each(function(index) {
 		  	var imageAlt = $(this).attr('alt');
 			if(!(typeof attr !== typeof undefined && attr !== false)) {
@@ -26,10 +60,6 @@ $(document).ready(function() {
 			      $(".footer").append("<div style='color:rgba(255, 255, 255, 0.5); float:right;' class='build_time'>Version build on " + data["git.build.time"] + "</div>");
 			      FOOTER_ADDED = true;
 			});
-		}
-
-		if(/branding=false/.test(search)) {
-			$(".roda").css("background-color", "white");
 		}
 	});
 });
