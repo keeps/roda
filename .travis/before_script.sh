99623fc3d34f5d98f16c4d9442e8da5d47797cf0
Luis Faria <lfaria@keep.pt>
2018-09-19 11:04:46 +0100

    using clam[d]scan dummy and ensuring cache permissions

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -2,22 +2,18 @@
 
 set -x
 
-# install dependencies
+# install siegfried
 curl https://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
 echo "deb http://dl.bintray.com/siegfried/debian wheezy main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get -qq update
 sudo apt-get -qq install siegfried -y
 sudo sf -update
-sudo apt-get -qq install clamav clamav-daemon -y
 
-# Enable cache of clamav databases to improve build time
-sudo mv /usr/bin/clamscan /usr/bin/clamscan-real
+
+# Install dummy clam[d]scan
 sudo cp .travis/clamscan /usr/bin/clamscan
-sudo chmod a+rx /usr/bin/clamscan
-sudo mkdir -p "$CLAMAV_DATABASE"
-sudo chown clamav:clamav "$CLAMAV_DATABASE"
-sudo freshclam --datadir=$CLAMAV_DATABASE
-sudo -u clamav chmod -R a+r "$CLAMAV_DATABASE"
+sudo cp .travis/clamscan /usr/bin/clamdscan
+sudo chmod a+rx /usr/bin/clamscan /usr/bin/clamdscan
 
 # decrypt maven setting.xml
 if [[ ! -z "$encrypted_a8a9ca6bf122_key" ]]; then
