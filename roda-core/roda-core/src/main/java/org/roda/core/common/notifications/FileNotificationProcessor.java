d264d588879e98b95e1e8424c7a0dc8ccbe8d6fd
006627 <g.santos.barros@gmail.com>
2020-02-20 16:24:45 +0000

    Fix file process notification

--- roda-core/roda-core/src/main/java/org/roda/core/common/notifications/FileNotificationProcessor.java
+++ roda-core/roda-core/src/main/java/org/roda/core/common/notifications/FileNotificationProcessor.java
@@ -7,23 +7,23 @@
  */
 package org.roda.core.common.notifications;
 
+import org.roda.core.data.common.RodaConstants;
+import org.roda.core.data.exceptions.RODAException;
+import org.roda.core.data.utils.JsonUtils;
+import org.roda.core.data.v2.jobs.Job;
+import org.roda.core.data.v2.notifications.Notification;
+import org.roda.core.data.v2.notifications.NotificationState;
+import org.roda.core.model.ModelService;
+import org.roda.core.model.utils.ModelUtils;
+import org.roda.core.storage.DirectResourceAccess;
+import org.roda.core.storage.fs.FSUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-
-import org.roda.core.RodaCoreFactory;
-import org.roda.core.data.common.RodaConstants;
-import org.roda.core.data.exceptions.AlreadyExistsException;
-import org.roda.core.data.exceptions.GenericException;
-import org.roda.core.data.utils.JsonUtils;
-import org.roda.core.data.v2.jobs.Job;
-import org.roda.core.data.v2.notifications.NotificationState;
-import org.roda.core.data.v2.notifications.Notification;
-import org.roda.core.model.ModelService;
-import org.roda.core.storage.fs.FSUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FileNotificationProcessor implements NotificationProcessor {
   private static final Logger LOGGER = LoggerFactory.getLogger(FileNotificationProcessor.class);
@@ -50,22 +50,19 @@ public class FileNotificationProcessor implements NotificationProcessor {
         Path trimmedDropPath = Paths.get(dropPath.substring(7));
 
         if (FSUtils.isDirectory(trimmedDropPath)) {
-          try {
+          try (DirectResourceAccess jobAccess = model.getStorage().getDirectAccess(ModelUtils.getJobStoragePath(job.getId()));
+               DirectResourceAccess jobReportAccess = model.getStorage().getDirectAccess(ModelUtils.getJobReportsStoragePath(job.getId()))) {
+
             Path jobPath = FSUtils.createDirectory(trimmedDropPath, job.getId());
 
-            Path jobFilePath = RodaCoreFactory.getStoragePath().resolve(RodaConstants.STORAGE_CONTAINER_JOB)
-              .resolve(job.getId() + RodaConstants.JOB_FILE_EXTENSION);
-            FSUtils.copy(jobFilePath, jobPath.resolve(job.getId() + RodaConstants.JOB_FILE_EXTENSION), true);
-
-            Path jobReportPath = RodaCoreFactory.getStoragePath().resolve(RodaConstants.STORAGE_CONTAINER_JOB_REPORT)
-              .resolve(job.getId());
-            FSUtils.copy(jobReportPath, jobPath.resolve(RodaConstants.RODA_OBJECT_REPORTS), true);
+            FSUtils.copy(jobAccess.getPath(), jobPath.resolve(job.getId() + RodaConstants.JOB_FILE_EXTENSION), true);
+            FSUtils.copy(jobReportAccess.getPath(), jobPath.resolve(RodaConstants.RODA_OBJECT_REPORTS), true);
 
             FSUtils.createFile(jobPath, ".ready");
 
             LOGGER.debug("Notification sent");
             notification.setState(NotificationState.COMPLETED);
-          } catch (AlreadyExistsException | GenericException | IOException e) {
+          } catch (RODAException | IOException e) {
             LOGGER.warn("Notification not sent", e);
           }
         } else {
