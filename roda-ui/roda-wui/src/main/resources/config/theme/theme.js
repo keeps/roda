46ddb441a1b062d25690b7ace78e8756582d4df3
mguimaraes <mguimaraes@keep.pt>
2020-09-15 18:09:27 +0100

    Fix client-side cross-site scripting

--- roda-ui/roda-wui/src/main/resources/config/theme/theme.js
+++ roda-ui/roda-wui/src/main/resources/config/theme/theme.js
@@ -1,6 +1,6 @@
 var FOOTER_ADDED = false;
 
-$(document).ready(function() {
+$(document).ready(function () {
     // getUrlParameters function based on
     // from https://stackoverflow.com/a/2880929/1483200
     var urlParams = (function () {
@@ -21,45 +21,49 @@ $(document).ready(function() {
 
     // keep branding=false retrocompatibility
     if (urlParams['branding'] === 'false') {
-        urlParams['branding'] = 'nobranding.css';
+        urlParams['branding'] = 'nobranding';
     }
+
+    let brandingsToInsert = [];
 
     if ((typeof urlParams['branding']) === 'string') {
         var brandings = urlParams['branding'].split(',');
 
-        brandings.forEach(function(branding){
-            if (branding.endsWith("css")) {
-                $(document).on('DOMNodeInserted', ".footer", function(e) {
-                    $("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=' + branding + '">');
-                });
-            } else {
-                // allow branding to be specified without extension
-                $(document).on('DOMNodeInserted', ".footer", function(e) {
-                    $("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=' + branding + '.css">');
-                });
+        brandings.forEach(function (branding) {
+            if (/^([a-z0-9]+)$/.test(branding)) {
+                brandingsToInsert.push(branding.concat(".css"));
             }
         });
     }
 
-	// necessary to pass on the accessibility test
-	$(document).on('DOMNodeInserted', "thead", function(e) {
-		$("img").each(function(index) {
-		  	var imageAlt = $(this).attr('alt');
-			if(!(typeof attr !== typeof undefined && attr !== false)) {
-				$(this).attr('alt', 'img_alt');
-			}
-		});
-	});
+    $(document).on('DOMNodeInserted', ".gwt-HTML", function (e) {
+        elem = e.target;
+        if ($(elem).find('.footer').length > 0) {
+            brandingsToInsert.forEach(function (branding) {
+                $("head").append('<link rel="stylesheet" type="text/css" href="api/v1/theme?resource_id=' + branding + '">');
+            });
+        }
+    });
 
-	$(document).on('DOMNodeInserted', ".footer", function(e) {
-		elem = e.target;
+    // necessary to pass on the accessibility test
+    $(document).on('DOMNodeInserted', "thead", function (e) {
+        $("img").each(function (index) {
+            var imageAlt = $(this).attr('alt');
+            if (!(typeof attr !== typeof undefined && attr !== false)) {
+                $(this).attr('alt', 'img_alt');
+            }
+        });
+    });
 
-		if(!FOOTER_ADDED && $(elem).hasClass("footer")) {
-			var pathname = window.location.pathname;
-			$.get(pathname + "version.json", function(data) {
-			      $(".footer").append("<div style='color:rgba(255, 255, 255, 0.5); float:right;' class='build_time'>Version build on " + data["git.build.time"] + "</div>");
-			      FOOTER_ADDED = true;
-			});
-		}
-	});
+    $(document).on('DOMNodeInserted', '.gwt-HTML', function (e) {
+        elem = e.target;
+
+        if (!FOOTER_ADDED && $(elem).find('.footer').length > 0) {
+            var pathname = window.location.pathname;
+            $.get(pathname + "version.json", function (data) {
+                $(".footer").append("<div style='color:rgba(255, 255, 255, 0.5); float:right;' class='build_time'>Version build on " + data["git.build.time"] + "</div>");
+                FOOTER_ADDED = true;
+            });
+        }
+    });
 });
