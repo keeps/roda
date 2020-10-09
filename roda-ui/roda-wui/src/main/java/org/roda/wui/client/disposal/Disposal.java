f7082a1b667ef4691d4242208bc2f378d3e355c7
tfraga <tfraga@keep.pt>
2020-10-09 17:51:40 +0100

    wip: update menu bar

---
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/client/disposal/Disposal.java
@@ -0,0 +1,77 @@
+package org.roda.wui.client.disposal;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
+import org.roda.wui.client.browse.PreservationEvents;
+import org.roda.wui.client.common.UserLogin;
+import org.roda.wui.client.planning.*;
+import org.roda.wui.common.client.HistoryResolver;
+import org.roda.wui.common.client.tools.HistoryUtils;
+import org.roda.wui.common.client.widgets.HTMLWidgetWrapper;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Tiago Fraga <tfraga@keep.pt>
+ */
+public class Disposal {
+    public static final HistoryResolver RESOLVER = new HistoryResolver() {
+
+        @Override
+        public void resolve(List<String> historyTokens, AsyncCallback<Widget> callback) {
+            getInstance().resolve(historyTokens, callback);
+        }
+
+        @Override
+        public void isCurrentUserPermitted(AsyncCallback<Boolean> callback) {
+            UserLogin.getInstance().checkRoles(
+                    new HistoryResolver[] {DisposalPolicy.RESOLVER, DisposalConfirmations.RESOLVER, DisposalDestroyedRecords.RESOLVER}, false, callback);
+        }
+
+        @Override
+        public List<String> getHistoryPath() {
+            return Arrays.asList(getHistoryToken());
+        }
+
+        @Override
+        public String getHistoryToken() {
+            return "disposal";
+        }
+    };
+
+    private static Disposal instance = null;
+
+    /**
+     * Get the singleton instance
+     *
+     * @return the instance
+     */
+    public static Disposal getInstance() {
+        if (instance == null) {
+            instance = new Disposal();
+        }
+        return instance;
+    }
+
+    private boolean initialized;
+
+    private HTMLWidgetWrapper page;
+
+    private Disposal() {
+        initialized = false;
+    }
+
+
+    public void resolve(List<String> historyTokens, AsyncCallback<Widget> callback) {
+        if (historyTokens.isEmpty()) {
+            callback.onSuccess(page);
+        } else if (historyTokens.get(0).equals(DisposalPolicy.RESOLVER.getHistoryToken())) {
+            DisposalPolicy.getInstance().resolve(HistoryUtils.tail(historyTokens), callback);
+        } else if (historyTokens.get(0).equals(DisposalConfirmations.RESOLVER.getHistoryToken())) {
+            DisposalConfirmations.getInstance().resolve(HistoryUtils.tail(historyTokens), callback);
+        } else if (historyTokens.get(0).equals(DisposalDestroyedRecords.RESOLVER.getHistoryToken())) {
+            DisposalDestroyedRecords.RESOLVER.resolve(HistoryUtils.tail(historyTokens), callback);
+        }
+    }
+}
