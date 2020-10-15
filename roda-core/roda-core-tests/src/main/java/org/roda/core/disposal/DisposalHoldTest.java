b8d9cb66fe5aa2789cfac4336eebfde87cdb289f
mguimaraes <mguimaraes@keep.pt>
2020-10-15 17:11:27 +0100

    Disposal Policy UI
    wip disposal confirmation
    
    wip disposal roles
    
    wip: Disposal Policy UI
    
    wip: Disposal Policy UI
    
    wip
    
    wip
    
    wip
    
    wip: disposal policy buttons
    
    wip: disposalConfirmationTest
    
    disposal roles
    
    wip: add permission to disposal policy buttons
    
    wip: disposalPolicy add permissions control
    
    wip: disposalPolicy add permissions control
    
    wip: disposalPolicy add permissions control buttons

--- roda-core/roda-core-tests/src/main/java/org/roda/core/disposal/DisposalHoldTest.java
+++ roda-core/roda-core-tests/src/main/java/org/roda/core/disposal/DisposalHoldTest.java
@@ -63,7 +63,7 @@ public class DisposalHoldTest {
   @AfterClass
   public void tearDown() throws Exception {
     RodaCoreFactory.shutdown();
-    FSUtils.deletePath(basePath);
+    //FSUtils.deletePath(basePath);
   }
 
   @AfterMethod
