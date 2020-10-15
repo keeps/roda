a7a07ec2f1bb8252a01a5bef10d561791d09d845
tfraga <tfraga@keep.pt>
2020-10-15 18:06:24 +0100

    Disposal Policy UI

--- roda-ui/roda-wui/src/main/java/org/roda/wui/client/disposal/Disposal.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/client/disposal/Disposal.java
@@ -65,8 +65,7 @@ public class Disposal {
   public void resolve(List<String> historyTokens, AsyncCallback<Widget> callback) {
     if (historyTokens.isEmpty()) {
       callback.onSuccess(page);
-    }
-    if (historyTokens.get(0).equals(DisposalPolicy.RESOLVER.getHistoryToken())) {
+    } else if (historyTokens.get(0).equals(DisposalPolicy.RESOLVER.getHistoryToken())) {
       DisposalPolicy.RESOLVER.resolve(HistoryUtils.tail(historyTokens), callback);
     } else if (historyTokens.get(0).equals(DisposalConfirmations.RESOLVER.getHistoryToken())) {
       DisposalConfirmations.getInstance().resolve(HistoryUtils.tail(historyTokens), callback);
