1ee674bf55fbab920aedd503c6bbd0b2290ca61e
Miguel Guimarães <mguimaraes@keep.pt>
2021-02-23 14:34:09 +0000

    wip shallow features

--- roda-core/roda-core/src/main/java/org/roda/core/storage/protocol/ProtocolManagerFactory.java
+++ roda-core/roda-core/src/main/java/org/roda/core/storage/protocol/ProtocolManagerFactory.java
@@ -1,8 +1,8 @@
 package org.roda.core.storage.protocol;
 
-import org.roda.core.data.exceptions.GenericException;
-
 import java.net.URI;
+
+import org.roda.core.data.exceptions.GenericException;
 
 /**
  * @author Gabriel Barros <gbarros@keep.pt>
@@ -11,11 +11,11 @@ public class ProtocolManagerFactory {
   public static ProtocolManager createProtocolManager(URI connectionString) throws GenericException {
     String scheme = connectionString.getScheme();
     ProtocolManager protocolManager;
-    if(scheme.equals("https") || scheme.equals("http")) {
+    if (scheme.equals("https") || scheme.equals("http")) {
       protocolManager = new HTTPProtocolManager(connectionString);
-    } else if (scheme.equals("nfs")){
+    } else if (scheme.equals("nfs")) {
       protocolManager = new NFSProtocolManager(connectionString);
-    } else if (scheme.equals("file")){
+    } else if (scheme.equals("file")) {
       protocolManager = new FileProtocolManager(connectionString);
     } else {
       throw new GenericException("Protocol " + scheme + "not implemented");
