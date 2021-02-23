7836a7e60cdf172c5068ee6f53ea33bb1ac66c09
Gabriel Barros <g.santos.barros@gmail.com>
2021-02-23 09:21:47 +0000

    resolve temporary AIP-S

--- roda-core/roda-core/src/main/java/org/roda/core/storage/protocol/HTTPProtocolManager.java
+++ roda-core/roda-core/src/main/java/org/roda/core/storage/protocol/HTTPProtocolManager.java
@@ -1,8 +1,12 @@
 package org.roda.core.storage.protocol;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.roda.core.storage.DefaultBinary;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,6 +15,9 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URI;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
 import java.util.stream.Collectors;
 
 /**
@@ -45,6 +52,20 @@ public class HTTPProtocolManager implements ProtocolManager{
       return HttpURLConnection.HTTP_OK == responseCode;
     } catch (IOException e) {
       return false;
+    }
+  }
+
+  @Override
+  public void downloadResource(Path target) {
+    Path output = target.resolve(FilenameUtils.getName(connectionString.getPath()));
+    ReadableByteChannel readableByteChannel = Channels.newChannel(getInputStream());
+    try {
+      FileOutputStream fileOutputStream = new FileOutputStream(output.toString());
+      fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
