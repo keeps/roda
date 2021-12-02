13c8d28d8aa44d738e68e2c1867109a61ac27226
Gabriel Barros <g.santos.barros@gmail.com>
2021-12-02 17:51:49 +0000

    Updates AIP when metadata changes

--- roda-core/roda-core/src/main/java/org/roda/core/plugins/plugins/internal/synchronization/bundle/CreateRodaEntityPackagePlugin.java
+++ roda-core/roda-core/src/main/java/org/roda/core/plugins/plugins/internal/synchronization/bundle/CreateRodaEntityPackagePlugin.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import org.roda.core.RodaCoreFactory;
