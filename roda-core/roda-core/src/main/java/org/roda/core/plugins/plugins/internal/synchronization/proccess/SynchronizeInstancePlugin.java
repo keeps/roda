4916d543e56657c1dbd7aa0585b35ceb0855c6bf
Shahzod Yusupov <syusupov@keep.pt>
2022-03-15 16:01:27 +0000

    Changes in the interface of Synchronization process

--- roda-core/roda-core/src/main/java/org/roda/core/plugins/plugins/internal/synchronization/proccess/SynchronizeInstancePlugin.java
+++ roda-core/roda-core/src/main/java/org/roda/core/plugins/plugins/internal/synchronization/proccess/SynchronizeInstancePlugin.java
@@ -1,7 +1,11 @@
 package org.roda.core.plugins.plugins.internal.synchronization.proccess;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.roda.core.RodaCoreFactory;
 import org.roda.core.common.SyncUtils;
@@ -9,7 +13,6 @@ import org.roda.core.data.common.RodaConstants;
 import org.roda.core.data.exceptions.GenericException;
 import org.roda.core.data.exceptions.InvalidParameterException;
 import org.roda.core.data.exceptions.NotFoundException;
-import org.roda.core.data.exceptions.RODAException;
 import org.roda.core.data.v2.IsRODAObject;
 import org.roda.core.data.v2.LiteOptionalWithCause;
 import org.roda.core.data.v2.Void;
@@ -32,8 +35,12 @@ import org.roda.core.plugins.PluginException;
 import org.roda.core.plugins.RODAProcessingLogic;
 import org.roda.core.plugins.orchestrate.JobPluginInfo;
 import org.roda.core.plugins.plugins.PluginHelper;
-import org.roda.core.plugins.plugins.internal.synchronization.SyncBundleHelper;
-import org.roda.core.plugins.plugins.internal.synchronization.packages.*;
+import org.roda.core.plugins.plugins.internal.synchronization.packages.AipPackagePlugin;
+import org.roda.core.plugins.plugins.internal.synchronization.packages.DipPackagePlugin;
+import org.roda.core.plugins.plugins.internal.synchronization.packages.JobPackagePlugin;
+import org.roda.core.plugins.plugins.internal.synchronization.packages.PreservationAgentPackagePlugin;
+import org.roda.core.plugins.plugins.internal.synchronization.packages.RepositoryEventPackagePlugin;
+import org.roda.core.plugins.plugins.internal.synchronization.packages.RiskIncidencePackagePlugin;
 import org.roda.core.storage.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -98,7 +105,7 @@ public class SynchronizeInstancePlugin extends AbstractPlugin<Void> {
 
   @Override
   public boolean areParameterValuesValid() {
-    return false;
+    return true;
   }
 
   @Override
