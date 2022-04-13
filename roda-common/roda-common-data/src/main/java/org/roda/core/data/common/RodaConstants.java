ad5bf1fb6cf9ff055247a5285174d78a6cfdf4d4
Shahzod Yusupov <36776778+47Balboa@users.noreply.github.com>
2022-04-13 17:10:25 +0100

    Added filters to remove jobs with created and pending_approval status from joblist to sync (#2019)

--- roda-common/roda-common-data/src/main/java/org/roda/core/data/common/RodaConstants.java
+++ roda-common/roda-common-data/src/main/java/org/roda/core/data/common/RodaConstants.java
@@ -935,6 +935,7 @@ public final class RodaConstants {
   public static final String JOB_START_DATE = "startDate";
   public static final String JOB_END_DATE = "endDate";
   public static final String JOB_STATE = "state";
+  public static final String JOB_EXECUTION_TYPE_APPROVAL = "APPROVAL";
   public static final String JOB_PARALLELISM = "parallelism";
   public static final String JOB_PRIORITY = "priority";
   public static final String JOB_STATE_DETAILS = "stateDetails";
