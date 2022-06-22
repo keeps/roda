e7c0363078d1bb663d06dd80b1e40115b653f0c5
Shahzod Yusupov <shahzod1997@hotmail.com>
2022-06-22 15:08:54 +0100

    Added distributed instance id in error message

--- roda-core/roda-core/src/main/java/org/roda/core/model/ModelService.java
+++ roda-core/roda-core/src/main/java/org/roda/core/model/ModelService.java
@@ -4201,7 +4201,7 @@ public class ModelService extends ModelObservable {
     try {
       binary = storage.getBinary(distributedInstancePath);
     } catch (NotFoundException e) {
-      throw new GenericException("Could not find the distributed instance", e);
+      throw new GenericException("Could not find the distributed instance" + distributedInstanceId, e);
     }
     DistributedInstance ret;
 
