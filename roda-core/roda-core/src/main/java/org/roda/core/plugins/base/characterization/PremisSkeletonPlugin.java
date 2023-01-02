9b1cada6269dc903107a8569ddf4ae04f3f2a63c
António Lindo <alindo@keep.pt>
2023-01-02 11:34:34 +0000

    Fixed import

--- roda-core/roda-core/src/main/java/org/roda/core/plugins/base/characterization/PremisSkeletonPlugin.java
+++ roda-core/roda-core/src/main/java/org/roda/core/plugins/base/characterization/PremisSkeletonPlugin.java
@@ -43,6 +43,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
