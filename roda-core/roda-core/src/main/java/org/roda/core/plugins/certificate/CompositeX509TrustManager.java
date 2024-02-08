19349390097a47dc352ca754f61977ed3266299c
Gabriel Barros <g.santos.barros@gmail.com>
2024-02-08 09:33:40 +0000

    Setting version 5.4.0

--- roda-core/roda-core/src/main/java/org/roda/core/plugins/certificate/CompositeX509TrustManager.java
+++ roda-core/roda-core/src/main/java/org/roda/core/plugins/certificate/CompositeX509TrustManager.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE file at the root of the source
+ * tree and available online at
+ *
+ * https://github.com/keeps/roda
+ */
 package org.roda.core.plugins.certificate;
 
 import java.security.cert.CertificateException;
