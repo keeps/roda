57b4e4d00c4641d93d9134d8531aec6479562f26
Miguel Guimarães <mguimaraes@keep.pt>
2024-03-27 11:10:22 +0000

    Use buildx to create attestation manifests for docker images

---
+++ docker/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+VERSION=$(mvn -f "$PROJECT_DIR"/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+mvn -f "$PROJECT_DIR"/pom.xml clean package -DskipTests
+
+# Clean up target folder
+rm -rf "$SCRIPT_DIR"/target/*
+
+# Copy target
+cp -r "$PROJECT_DIR"/roda-ui/roda-wui/target/roda-wui-"$VERSION"/* "$SCRIPT_DIR"/target/
+
+docker build -t keeps/roda:latest -t keeps/roda:"$VERSION" .
