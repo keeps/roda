bf166f48d98bb6e529f2d21d583d19398b8be2c3
António Lindo <alindo@keep.pt>
2024-04-11 10:20:08 +0100

    wip jobs

--- roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/JobsResource.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/JobsResource.java
@@ -2,20 +2,38 @@ package org.roda.wui.api.v2;
 
 import java.util.ArrayList;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.glassfish.jersey.server.JSONP;
 import org.roda.core.RodaCoreFactory;
 import org.roda.core.data.common.RodaConstants;
 import org.roda.core.data.exceptions.RODAException;
+import org.roda.core.data.v2.EntityResponse;
+import org.roda.core.data.v2.StreamResponse;
 import org.roda.core.data.v2.index.select.SelectedItems;
 import org.roda.core.data.v2.index.select.SelectedItemsList;
 import org.roda.core.data.v2.jobs.Job;
 import org.roda.core.data.v2.jobs.JobUserDetails;
 import org.roda.core.data.v2.jobs.Jobs;
+import org.roda.core.data.v2.jobs.Reports;
 import org.roda.core.data.v2.log.LogEntryState;
 import org.roda.core.data.v2.user.User;
 import org.roda.core.model.ModelService;
 import org.roda.core.model.utils.UserUtility;
 import org.roda.wui.api.controllers.BrowserHelper;
 import org.roda.wui.api.controllers.JobsHelper;
+import org.roda.wui.api.v1.utils.ApiResponseMessage;
+import org.roda.wui.api.v1.utils.ApiUtils;
+import org.roda.wui.api.v1.utils.ExtraMediaType;
 import org.roda.wui.api.v2.exceptions.RESTException;
 import org.roda.wui.client.services.JobsService;
 import org.roda.wui.common.ControllerAssistant;
@@ -299,5 +317,40 @@ public class JobsResource implements JobsService {
       throw new RESTException(e);
     }
     return null;
+  }
+
+  @Override
+  public Reports listJobReports(String jobId, boolean justFailed, String start, String limit) {
+    // get user
+    User user = UserUtility.getApiUser(request);
+
+    try {
+      // delegate action to controller
+      return JobsHelper.getJobReportsFromIndexResult(user, jobId, justFailed, start, limit, new ArrayList<>());
+    } catch (RODAException e) {
+      throw new RESTException(e);
+    }
+  }
+
+  @GET
+  @Path("/{" + RodaConstants.API_PATH_PARAM_JOB_ID + "}/attachment/{" + RodaConstants.API_PATH_PARAM_JOB_ATTACHMENT_ID
+    + "}")
+  @Produces({ExtraMediaType.APPLICATION_ZIP})
+  @JSONP(callback = RodaConstants.API_QUERY_DEFAULT_JSONP_CALLBACK, queryParam = RodaConstants.API_QUERY_KEY_JSONP_CALLBACK)
+  @Operation(summary = "Get attachment", description = "Gets the attachments of a job", responses = {
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = Job.class))),
+    @ApiResponse(responseCode = "404", description = "Not found", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ApiResponseMessage.class)))})
+  public Response retrieveJobAttachment(
+    @Parameter(description = "The ID of the existing job", required = true) @PathParam(RodaConstants.API_PATH_PARAM_JOB_ID) String jobId,
+    @Parameter(description = "The ID of the existing attachment", required = true) @PathParam(RodaConstants.API_PATH_PARAM_JOB_ATTACHMENT_ID) String attachmentId,
+    @Parameter(description = "Choose format in which to get the attachments", schema = @Schema(allowableValues = {
+      ExtraMediaType.APPLICATION_ZIP}, defaultValue = ExtraMediaType.APPLICATION_ZIP)) @QueryParam(RodaConstants.API_QUERY_KEY_ACCEPT_FORMAT) String acceptFormat)
+    throws RODAException {
+
+    // get user
+    User user = UserUtility.getApiUser(request);
+
+    EntityResponse response = org.roda.wui.api.controllers.Jobs.retrieveJobAttachment(user, jobId, attachmentId);
+    return ApiUtils.okResponse((StreamResponse) response);
   }
 }
