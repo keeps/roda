5baa0b067af720eb4af75a916e7b74260024a16a
Carlos Afonso <cafonso@keep.pt>
2025-04-17 16:01:12 +0100

    Fixed Disposal confirmation ui

--- roda-ui/roda-wui/src/main/java/org/roda/wui/client/disposal/DisposalConfirmations.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/client/disposal/DisposalConfirmations.java
@@ -12,13 +12,10 @@ import java.util.List;
 import org.roda.core.data.v2.disposal.confirmation.DisposalConfirmation;
 import org.roda.wui.client.common.UserLogin;
 import org.roda.wui.client.common.actions.DisposalConfirmationActions;
-import org.roda.wui.client.common.actions.model.ActionableObject;
-import org.roda.wui.client.common.actions.widgets.ActionableWidgetBuilder;
 import org.roda.wui.client.common.lists.DisposalConfirmationList;
 import org.roda.wui.client.common.lists.utils.AsyncTableCellOptions;
 import org.roda.wui.client.common.lists.utils.ListBuilder;
 import org.roda.wui.client.common.search.SearchWrapper;
-import org.roda.wui.client.common.utils.SidebarUtils;
 import org.roda.wui.client.disposal.confirmations.CreateDisposalConfirmation;
 import org.roda.wui.client.disposal.confirmations.ShowDisposalConfirmation;
 import org.roda.wui.common.client.HistoryResolver;
@@ -31,7 +28,6 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import config.i18n.client.ClientMessages;
@@ -69,31 +65,21 @@ public class DisposalConfirmations extends Composite {
   FlowPanel disposalConfirmationDescription;
   @UiField(provided = true)
   SearchWrapper searchWrapper;
-  @UiField
-  SimplePanel actionsSidebar;
-  @UiField
-  FlowPanel contentFlowPanel;
-  @UiField
-  FlowPanel sidebarFlowPanel;
 
   /**
    * Create a disposal confirmation page
    */
   public DisposalConfirmations() {
+
+    final DisposalConfirmationActions confirmationActions = DisposalConfirmationActions.get();
+
     ListBuilder<DisposalConfirmation> disposalConfirmationListBuilder = new ListBuilder<>(
       () -> new DisposalConfirmationList(),
-      new AsyncTableCellOptions<>(DisposalConfirmation.class, "Disposal_confirmations").bindOpener()
+      new AsyncTableCellOptions<>(DisposalConfirmation.class, "Disposal_confirmations").bindOpener().withActionable(confirmationActions)
         .withAutoUpdate(5000));
 
     searchWrapper = new SearchWrapper(false).createListAndSearchPanel(disposalConfirmationListBuilder);
-
     initWidget(uiBinder.createAndBindUi(this));
-
-    final DisposalConfirmationActions confirmationActions = DisposalConfirmationActions.get();
-    SidebarUtils.toggleSidebar(contentFlowPanel, sidebarFlowPanel, confirmationActions.hasAnyRoles());
-    actionsSidebar.setWidget(new ActionableWidgetBuilder<>(confirmationActions)
-      .buildListWithObjects(new ActionableObject<>(DisposalConfirmation.class)));
-
     disposalConfirmationDescription.add(new HTMLWidgetWrapper("DisposalConfirmationDescription.html"));
   }
 
