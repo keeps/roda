c7810990407a7e55c9187690f7dfebd357c0bb5d
Gabriel Barros <g.santos.barros@gmail.com>
2025-06-04 15:51:55 +0100

    Determine which storage service to be used based on transaction log

--- roda-core/roda-core/src/main/java/org/roda/core/repository/transaction/TransactionalStoragePathRepository.java
+++ roda-core/roda-core/src/main/java/org/roda/core/repository/transaction/TransactionalStoragePathRepository.java
@@ -1,12 +1,19 @@
 package org.roda.core.repository.transaction;
 
+import org.roda.core.entity.transaction.TransactionLog;
 import org.roda.core.entity.transaction.TransactionalStoragePathOperationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Gabriel Barros <gbarros@keep.pt>
  */
 @Repository
-public interface TransactionalStoragePathRepository extends JpaRepository<TransactionalStoragePathOperationLog, Long> {
+public interface TransactionalStoragePathRepository extends JpaRepository<TransactionalStoragePathOperationLog, UUID> {
+    // find All By transaction id and storage path
+    List<TransactionalStoragePathOperationLog> findByTransactionLogAndStoragePath(TransactionLog transactionLog, String storagePath);
+
 }
