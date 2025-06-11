ad2f02c604e3a1c6990b69426a51af6e7dc7be0f
Miguel Guimarães <mguimaraes@keep.pt>
2025-06-11 12:29:08 +0100

    Refactor notifications controller to use RequestHandler

--- roda-ui/roda-wui/src/main/java/org/roda/wui/client/services/NotificationRestService.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/client/services/NotificationRestService.java
@@ -1,9 +1,9 @@
 package org.roda.wui.client.services;
 
+import org.roda.core.data.v2.generics.StringResponse;
 import org.roda.core.data.v2.notifications.Notification;
 import org.roda.core.data.v2.notifications.NotificationAcknowledgeRequest;
 import org.roda.wui.api.v2.exceptions.model.ErrorResponseMessage;
-import org.roda.wui.api.v2.model.GenericOkResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +27,8 @@ public interface NotificationRestService extends RODAEntityRestService<Notificat
   Notification getNotification(@PathVariable(name = "id") String notificationId);
 
   @RequestMapping(method = RequestMethod.POST, path = "/acknowledge", produces = MediaType.APPLICATION_JSON_VALUE)
-  @Operation(summary = "Acknowledge notification", description = "Acknowledges a notification", requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = NotificationAcknowledgeRequest.class))), responses = {
-    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = GenericOkResponse.class))),
+  @Operation(summary = "Acknowledge notification", description = "Acknowledges a notification", requestBody = @RequestBody(required = true, content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = NotificationAcknowledgeRequest.class))), responses = {
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = StringResponse.class))),
     @ApiResponse(responseCode = "404", description = "Not found", content = @Content(schema = @Schema(implementation = ErrorResponseMessage.class)))})
-  String acknowledgeNotification(NotificationAcknowledgeRequest notificationAcknowledgeRequest);
+  StringResponse acknowledgeNotification(NotificationAcknowledgeRequest notificationAcknowledgeRequest);
 }
