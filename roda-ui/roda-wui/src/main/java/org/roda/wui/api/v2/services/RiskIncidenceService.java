96f7e7a6e3c27861c7bd6de6f906a8d161bc3419
Miguel Guimarães <mguimaraes@keep.pt>
2025-06-12 15:23:03 +0100

    Refactor risk incidence controller to use RequestHandler

--- roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/services/RiskIncidenceService.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/services/RiskIncidenceService.java
@@ -1,11 +1,8 @@
 package org.roda.wui.api.v2.services;
 
-import static org.roda.wui.api.v2.utils.CommonServicesUtils.createAndExecuteInternalJob;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import org.roda.core.RodaCoreFactory;
 import org.roda.core.data.common.RodaConstants;
 import org.roda.core.data.exceptions.AuthorizationDeniedException;
 import org.roda.core.data.exceptions.GenericException;
@@ -16,11 +13,10 @@ import org.roda.core.data.v2.index.select.SelectedItems;
 import org.roda.core.data.v2.jobs.Job;
 import org.roda.core.data.v2.risks.RiskIncidence;
 import org.roda.core.data.v2.user.User;
+import org.roda.core.model.ModelService;
 import org.roda.core.plugins.base.maintenance.DeleteRODAObjectPlugin;
 import org.roda.core.plugins.base.risks.UpdateIncidencesPlugin;
 import org.roda.wui.api.v2.utils.CommonServicesUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -28,13 +24,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RiskIncidenceService {
-  private static final Logger LOGGER = LoggerFactory.getLogger(RiskIncidenceService.class);
 
   public Job deleteRiskIncidences(User user, DeleteRequest request)
     throws AuthorizationDeniedException, GenericException, RequestNotValidException, NotFoundException {
     Map<String, String> pluginParameters = new HashMap<>();
     pluginParameters.put(RodaConstants.PLUGIN_PARAMS_DETAILS, request.getDetails());
-    return createAndExecuteInternalJob("Delete risk incidences",
+    return CommonServicesUtils.createAndExecuteInternalJob("Delete risk incidences",
       CommonServicesUtils.convertSelectedItems(request.getItemsToDelete(), RiskIncidence.class),
       DeleteRODAObjectPlugin.class, user, pluginParameters, "Could not execute risk incidence delete action");
   }
@@ -52,13 +47,14 @@ public class RiskIncidenceService {
     pluginParameters.put(RodaConstants.PLUGIN_PARAMS_RISK_INCIDENCE_MITIGATED_BY, user.getName());
     pluginParameters.put(RodaConstants.PLUGIN_PARAMS_RISK_INCIDENCE_MITIGATED_DESCRIPTION, mitigatedDescription);
 
-    return createAndExecuteInternalJob("Update risk incidences", selected, UpdateIncidencesPlugin.class, user,
+    return CommonServicesUtils.createAndExecuteInternalJob("Update risk incidences", selected,
+      UpdateIncidencesPlugin.class, user,
       pluginParameters, "Could not execute risk incidence update action");
   }
 
-  public RiskIncidence updateRiskIncidence(RiskIncidence incidence)
+  public RiskIncidence updateRiskIncidence(ModelService modelService, RiskIncidence incidence)
     throws GenericException, AuthorizationDeniedException {
-    return RodaCoreFactory.getModelService().updateRiskIncidence(incidence, true);
+    return modelService.updateRiskIncidence(incidence, true);
   }
 
 }
