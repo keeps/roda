5d3575b3d0d30317e7b3e16cf8de181626b7c8a5
Miguel Guimarães <mguimaraes@keep.pt>
2025-06-12 14:55:45 +0100

    Refactor risk incidence controller to use RequestHandler

--- roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/controller/RiskIncidenceController.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/controller/RiskIncidenceController.java
@@ -6,9 +6,7 @@ import java.util.List;
 import org.roda.core.data.common.RodaConstants;
 import org.roda.core.data.exceptions.AuthorizationDeniedException;
 import org.roda.core.data.exceptions.GenericException;
-import org.roda.core.data.exceptions.NotFoundException;
 import org.roda.core.data.exceptions.RODAException;
-import org.roda.core.data.exceptions.RequestNotValidException;
 import org.roda.core.data.v2.generics.DeleteRequest;
 import org.roda.core.data.v2.generics.LongResponse;
 import org.roda.core.data.v2.index.CountRequest;
@@ -27,6 +25,7 @@ import org.roda.wui.api.v2.utils.ApiUtils;
 import org.roda.wui.api.v2.utils.CommonServicesUtils;
 import org.roda.wui.client.services.RiskIncidenceRestService;
 import org.roda.wui.common.ControllerAssistant;
+import org.roda.wui.common.RequestControllerAssistant;
 import org.roda.wui.common.model.RequestContext;
 import org.roda.wui.common.utils.RequestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,9 +53,11 @@ public class RiskIncidenceController implements RiskIncidenceRestService, Export
   @Autowired
   private IndexService indexService;
 
+  @Autowired
+  private RequestHandler requestHandler;
+
   @Override
   public RiskIncidence findByUuid(String uuid, String localeString) {
-    RequestContext requestContext = RequestUtils.parseHTTPRequest(request);
     final List<String> fieldsToReturn = Arrays.asList(RodaConstants.INDEX_UUID, RodaConstants.RISK_INCIDENCE_ID,
       RodaConstants.RISK_INCIDENCE_RISK_ID, RodaConstants.RISK_INCIDENCE_DESCRIPTION,
       RodaConstants.RISK_INCIDENCE_STATUS, RodaConstants.RISK_INCIDENCE_SEVERITY,
@@ -68,7 +69,6 @@ public class RiskIncidenceController implements RiskIncidenceRestService, Export
 
   @Override
   public IndexResult<RiskIncidence> find(@RequestBody FindRequest findRequest, String localeString) {
-    RequestContext requestContext = RequestUtils.parseHTTPRequest(request);
     return indexService.find(RiskIncidence.class, findRequest, localeString);
   }
 
@@ -84,50 +84,34 @@ public class RiskIncidenceController implements RiskIncidenceRestService, Export
 
   @Override
   public List<String> suggest(SuggestRequest suggestRequest) {
-    RequestContext requestContext = RequestUtils.parseHTTPRequest(request);
     return indexService.suggest(suggestRequest, RiskIncidence.class);
   }
 
   @Override
   public Job deleteRiskIncidences(@RequestBody DeleteRequest deleteRequest) {
-    final ControllerAssistant controllerAssistant = new ControllerAssistant() {};
-    RequestContext requestContext = RequestUtils.parseHTTPRequest(request);
-    LogEntryState state = LogEntryState.SUCCESS;
-
-    try {
-      controllerAssistant.checkRoles(requestContext.getUser());
-      return riskIncidenceService.deleteRiskIncidences(requestContext.getUser(), deleteRequest);
-    } catch (RODAException e) {
-      state = LogEntryState.FAILURE;
-      throw new RESTException(e);
-    } finally {
-      // register action
-      controllerAssistant.registerAction(requestContext, state, RodaConstants.CONTROLLER_SELECTED_PARAM,
-        deleteRequest.getItemsToDelete(), RodaConstants.CONTROLLER_DETAILS_PARAM, deleteRequest.getDetails());
-    }
+    return requestHandler.processRequestWithTransaction(new RequestHandler.RequestProcessor<Job>() {
+      @Override
+      public Job process(RequestContext requestContext, RequestControllerAssistant controllerAssistant)
+        throws RODAException, RESTException {
+        controllerAssistant.setParameters(RodaConstants.CONTROLLER_SELECTED_PARAM, deleteRequest.getItemsToDelete(),
+          RodaConstants.CONTROLLER_DETAILS_PARAM, deleteRequest.getDetails());
+        return riskIncidenceService.deleteRiskIncidences(requestContext.getUser(), deleteRequest);
+      }
+    });
   }
 
   @Override
   public Job updateMultipleIncidences(@RequestBody UpdateRiskIncidences selected) {
-    final ControllerAssistant controllerAssistant = new ControllerAssistant() {};
-    RequestContext requestContext = RequestUtils.parseHTTPRequest(request);
-    LogEntryState state = LogEntryState.SUCCESS;
-
-    try {
-      controllerAssistant.checkRoles(requestContext.getUser());
-      return riskIncidenceService.updateMultipleIncidences(requestContext.getUser(),
-        CommonServicesUtils.convertSelectedItems(selected.getSelectedItems(), RiskIncidence.class),
-        selected.getStatus(), selected.getSeverity(), selected.getMitigatedDescription());
-    } catch (AuthorizationDeniedException e) {
-      state = LogEntryState.UNAUTHORIZED;
-      throw new RESTException(e);
-    } catch (GenericException | NotFoundException | RequestNotValidException e) {
-      state = LogEntryState.FAILURE;
-      throw new RESTException(e);
-    } finally {
-      // register action
-      controllerAssistant.registerAction(requestContext, state, RodaConstants.CONTROLLER_SELECTED_PARAM, selected);
-    }
+    return requestHandler.processRequestWithTransaction(new RequestHandler.RequestProcessor<Job>() {
+      @Override
+      public Job process(RequestContext requestContext, RequestControllerAssistant controllerAssistant)
+        throws RODAException, RESTException {
+        controllerAssistant.setParameters(RodaConstants.CONTROLLER_SELECTED_PARAM, selected);
+        return riskIncidenceService.updateMultipleIncidences(requestContext.getUser(),
+          CommonServicesUtils.convertSelectedItems(selected.getSelectedItems(), RiskIncidence.class),
+          selected.getStatus(), selected.getSeverity(), selected.getMitigatedDescription());
+      }
+    });
   }
 
   @Override
