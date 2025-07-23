3215c6a2cb477ce4c35b7b25034f2872dfdd5015
Gabriel Barros <g.santos.barros@gmail.com>
2025-07-23 11:36:35 +0100

    Improves database performance while handling consolidation logs

--- roda-core/roda-core/src/main/java/org/roda/core/config/MicrometerConfig.java
+++ roda-core/roda-core/src/main/java/org/roda/core/config/MicrometerConfig.java
@@ -2,6 +2,7 @@ package org.roda.core.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import io.micrometer.core.aop.CountedAspect;
@@ -11,6 +12,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 /**
  * @author Gabriel Barros <gbarros@keep.pt>
  */
+@Configuration
 @EnableAspectJAutoProxy
 public class MicrometerConfig {
   @Bean
@@ -20,7 +22,7 @@ public class MicrometerConfig {
   }
 
   @Bean
-  @ConditionalOnProperty(name = "management.metrics.micrometer.counted.aspect.enabled=", havingValue = "true", matchIfMissing = false)
+  @ConditionalOnProperty(name = "management.metrics.micrometer.counted.aspect.enabled", havingValue = "true", matchIfMissing = false)
   public CountedAspect countedAspect(MeterRegistry registry) {
     return new CountedAspect(registry);
   }
