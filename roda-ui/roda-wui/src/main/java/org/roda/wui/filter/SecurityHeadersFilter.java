7f7df9a3fd2a149df5b0dfee74264522cafb4a0f
Gabriel Barros <g.santos.barros@gmail.com>
2025-09-02 16:58:42 +0100

    make SecurityHeadersFilter fully configurable via properties

--- roda-ui/roda-wui/src/main/java/org/roda/wui/filter/SecurityHeadersFilter.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/filter/SecurityHeadersFilter.java
@@ -7,39 +7,67 @@
  */
 package org.roda.wui.filter;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.configuration.Configuration;
+import org.roda.core.RodaCoreFactory;
+import org.roda.core.common.RodaUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class SecurityHeadersFilter implements Filter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SecurityHeadersFilter.class);
+  private final Map<String, String> headers = new HashMap<>();
+  private final static String SECURITY_HEADER_PROPERTY_PREFIX = "ui.filter.security-headers[]";
+  private Boolean isInit = false;
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+  @Override
+  public void init(FilterConfig filterConfig) {
+  }
 
-        httpServletResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-        httpServletResponse.setHeader("Content-Security-Policy",
-        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com " +
-                "https://www.google-analytics.com https://www.gstatic.com http://127.0.0.1:9876; " +
-                "style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
-        httpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
-        httpServletResponse.setHeader("X-Permitted-Cross-Domain-Policies", "none");
-        httpServletResponse.setHeader("Feature-Policy", "camera 'none'; fullscreen 'self'; geolocation *; " +
-                "microphone 'self'");
-        httpServletResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
-        httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
-        httpServletResponse.setHeader("Referrer-Policy", "no-referrer");
-        httpServletResponse.setHeader("Permissions-Policy", "geolocation=(self)");
-
-        chain.doFilter(request, response);
+  private void initSecurityHeaders() {
+    if (isInit) {
+      return;
     }
 
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
+    final Configuration rodaConfig = RodaCoreFactory.getRodaConfiguration();
+    if (rodaConfig == null) {
+      LOGGER.info("RODA configuration not available yet. Delaying init of SecurityHeadersFilter.");
+    } else {
+      List<String> headersNames = RodaUtils.copyList(rodaConfig.getList(SECURITY_HEADER_PROPERTY_PREFIX));
 
-    @Override
-    public void destroy() {
+      for (String headersName : headersNames) {
+        String value = rodaConfig.getString(SECURITY_HEADER_PROPERTY_PREFIX + "." + headersName, null);
+        if (value != null) {
+          headers.put(headersName, value);
+        }
+      }
+      isInit = true;
+      LOGGER.info("SecurityHeadersFilter initialized with {} headers", headers.size());
     }
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+    throws IOException, ServletException {
+    initSecurityHeaders();
+    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+    headers.forEach(httpServletResponse::setHeader);
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {
+  }
 }
