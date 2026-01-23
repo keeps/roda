861734dd65cac4e09fafa683523bdf6db79205e5
Miguel Guimarães <mguimaraes@keep.pt>
2026-01-23 12:55:13 +0000

    Make SecurityHeadersFilter configurable via RODA properties #3586

--- roda-ui/roda-wui/src/main/java/org/roda/wui/filter/SecurityHeadersFilter.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/filter/SecurityHeadersFilter.java
@@ -8,6 +8,15 @@
 package org.roda.wui.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.configuration.Configuration;
+import org.roda.core.RodaCoreFactory;
+import org.roda.core.common.RodaUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -18,31 +27,45 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class SecurityHeadersFilter implements Filter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SecurityHeadersFilter.class);
+  private final Map<String, String> headers = new HashMap<>();
+  private final static String SECURITY_HEADER_PROPERTY_PREFIX = "ui.filter.security-headers[]";
+  private Boolean isInit = false;
+
+  @Override
+  public void init(FilterConfig filterConfig) {
+
+  }
+
+  private void initSecurityHeaders() {
+    if (isInit) {
+      return;
+    }
+
+    final Configuration rodaConfig = RodaCoreFactory.getRodaConfiguration();
+    if (rodaConfig == null) {
+      LOGGER.info("RODA configuration not available yet. Delaying init of SecurityHeadersFilter.");
+    } else {
+      List<String> headersNames = RodaUtils.copyList(rodaConfig.getList(SECURITY_HEADER_PROPERTY_PREFIX));
+
+      for (String headersName : headersNames) {
+        String value = rodaConfig.getString(SECURITY_HEADER_PROPERTY_PREFIX + "." + headersName, null);
+        if (value != null) {
+          headers.put(headersName, value);
+        }
+      }
+      isInit = true;
+      LOGGER.info("SecurityHeadersFilter initialized with {} headers", headers.size());
+    }
+  }
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
     throws IOException, ServletException {
+    initSecurityHeaders();
     HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-
-    httpServletResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-    httpServletResponse.setHeader("Content-Security-Policy",
-      "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com "
-        + "https://www.google-analytics.com https://www.gstatic.com http://127.0.0.1:9876 http://localhost:9876; "
-        + "style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';");
-    httpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
-    httpServletResponse.setHeader("X-Permitted-Cross-Domain-Policies", "none");
-    httpServletResponse.setHeader("Feature-Policy",
-      "camera 'none'; fullscreen 'self'; geolocation *; " + "microphone 'self'");
-    httpServletResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
-    httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
-    httpServletResponse.setHeader("Referrer-Policy", "no-referrer");
-    httpServletResponse.setHeader("Permissions-Policy", "geolocation=(self)");
-
+    headers.forEach(httpServletResponse::setHeader);
     chain.doFilter(request, response);
-  }
-
-  @Override
-  public void init(FilterConfig filterConfig) {
   }
 
   @Override
