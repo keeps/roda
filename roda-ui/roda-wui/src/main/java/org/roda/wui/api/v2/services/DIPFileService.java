55b84d65a59529402cab0ffb81487eb6f8672b2b
Luis Faria <lfaria@keep.pt>
2026-02-11 12:29:36 +0000

    Fixing streaming of files and generalizing for other storage providers.

--- roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/services/DIPFileService.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/api/v2/services/DIPFileService.java
@@ -7,6 +7,10 @@
  */
 package org.roda.wui.api.v2.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.roda.core.data.exceptions.AuthorizationDeniedException;
 import org.roda.core.data.exceptions.GenericException;
 import org.roda.core.data.exceptions.NotFoundException;
@@ -17,14 +21,13 @@ import org.roda.core.data.v2.StreamResponse;
 import org.roda.core.data.v2.ip.DIPFile;
 import org.roda.core.model.LiteRODAObjectFactory;
 import org.roda.core.model.ModelService;
+import org.roda.core.storage.Binary;
+import org.roda.core.storage.ContentPayload;
 import org.roda.core.storage.DirectResourceAccess;
 import org.roda.core.storage.RangeConsumesOutputStream;
+import org.roda.core.storage.SeekableContentPayload;
 import org.roda.wui.common.model.RequestContext;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 /**
  *
@@ -33,13 +36,21 @@ import java.util.Optional;
 @Service
 public class DIPFileService {
   public RangeConsumesOutputStream retrieveDIPFileRangeStream(RequestContext requestContext, DIPFile dipfile)
-    throws RequestNotValidException {
+      throws RequestNotValidException {
     ModelService model = requestContext.getModelService();
     if (!dipfile.isDirectory()) {
       final RangeConsumesOutputStream stream;
       try {
-        DirectResourceAccess directDIPFileAccess = model.getDirectAccess(dipfile);
-        stream = new RangeConsumesOutputStream(directDIPFileAccess.getPath());
+        Binary binary = model.getBinary(dipfile);
+        ContentPayload payload = binary.getContent();
+
+        if (payload instanceof SeekableContentPayload) {
+          SeekableContentPayload seekable = (SeekableContentPayload) payload;
+
+          stream = new RangeConsumesOutputStream(seekable, binary);
+        } else {
+          throw new RequestNotValidException("Range stream for file unsupported");
+        }
         return stream;
       } catch (RequestNotValidException | GenericException | AuthorizationDeniedException | NotFoundException e) {
         throw new RuntimeException(e);
@@ -50,7 +61,7 @@ public class DIPFileService {
   }
 
   public StreamResponse retrieveDIPFileStreamResponse(RequestContext requestContext, DIPFile dipFile)
-    throws GenericException, RequestNotValidException, NotFoundException, AuthorizationDeniedException {
+      throws GenericException, RequestNotValidException, NotFoundException, AuthorizationDeniedException {
     ModelService model = requestContext.getModelService();
 
     final ConsumesOutputStream stream;
