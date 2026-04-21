4b23e9c7b4a62503cb4d074b82dd6a13fd5db9bf
Eduardo Teixeira <58005905+eduardojst10@users.noreply.github.com>
2026-04-21 15:58:11 +0100

    ui updated for risk detailed layout. (#3651)
    Co-authored-by: Luis Faria <lfaria@keep.pt>

---
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/client/browse/tabs/BrowseRiskTabs.java
@@ -0,0 +1,46 @@
+package org.roda.wui.client.browse.tabs;
+
+import org.roda.core.data.common.RodaConstants;
+import org.roda.core.data.v2.index.filter.Filter;
+import org.roda.core.data.v2.index.filter.SimpleFilterParameter;
+import org.roda.core.data.v2.risks.IndexedRisk;
+import org.roda.core.data.v2.risks.RiskIncidence;
+import org.roda.wui.client.common.lists.RiskIncidenceList;
+import org.roda.wui.client.common.lists.utils.AsyncTableCellOptions;
+import org.roda.wui.client.common.lists.utils.ListBuilder;
+import org.roda.wui.client.common.search.SearchWrapper;
+
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.user.client.ui.Widget;
+
+public class BrowseRiskTabs extends Tabs {
+
+  public void init(IndexedRisk risk) {
+    createAndAddTab(SafeHtmlUtils.fromSafeConstant(messages.detailsTab()), new TabContentBuilder() {
+      ;
+      @Override
+      public Widget buildTabWidget() {
+        return new DetailsTab(risk);
+      }
+    });
+
+    createAndAddTab(SafeHtmlUtils.fromSafeConstant(messages.riskIncidences()), new TabContentBuilder() {
+      @Override
+      public Widget buildTabWidget() {
+        return buildIncidencesSearchWrapper(risk);
+      }
+    });
+  }
+
+  private Widget buildIncidencesSearchWrapper(IndexedRisk risk) {
+    String RISK_INCIDENCE_LIST_ID = "RiskShowPanel_riskIncidences";
+    Filter filter = new Filter(new SimpleFilterParameter(RodaConstants.RISK_INCIDENCE_RISK_ID, risk.getId()));
+
+    ListBuilder<RiskIncidence> listBuilder = new ListBuilder<>(() -> new RiskIncidenceList(),
+      new AsyncTableCellOptions<>(RiskIncidence.class, RISK_INCIDENCE_LIST_ID).withSummary(messages.riskIncidences())
+        .withFilter(filter).bindOpener().withSearchPlaceholder(messages.riskIncidenceRegisterSearchPlaceHolder()));
+
+    return new SearchWrapper(false).createListAndSearchPanel(listBuilder);
+  }
+
+}
--- roda-ui/roda-wui/src/main/java/org/roda/wui/client/planning/ShowRisk.java
+++ roda-ui/roda-wui/src/main/java/org/roda/wui/client/planning/ShowRisk.java
@@ -13,17 +13,19 @@ package org.roda.wui.client.planning;
 import java.util.List;
 
 import org.roda.core.data.v2.risks.IndexedRisk;
+import org.roda.wui.client.browse.tabs.BrowseRiskTabs;
+import org.roda.wui.client.common.BrowseRiskActionsToolbar;
+import org.roda.wui.client.common.NavigationToolbar;
 import org.roda.wui.client.common.NoAsyncCallback;
+import org.roda.wui.client.common.TitlePanel;
 import org.roda.wui.client.common.UserLogin;
 import org.roda.wui.client.common.actions.Actionable;
-import org.roda.wui.client.common.actions.RiskActions;
-import org.roda.wui.client.common.actions.model.ActionableObject;
-import org.roda.wui.client.common.actions.widgets.ActionableWidgetBuilder;
-import org.roda.wui.client.common.utils.SidebarUtils;
+import org.roda.wui.client.main.BreadcrumbUtils;
 import org.roda.wui.client.services.Services;
 import org.roda.wui.common.client.HistoryResolver;
 import org.roda.wui.common.client.tools.HistoryUtils;
 import org.roda.wui.common.client.tools.ListUtils;
+import org.roda.wui.common.client.tools.StringUtils;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.i18n.client.LocaleInfo;
@@ -31,8 +33,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import config.i18n.client.ClientMessages;
@@ -47,7 +48,7 @@ public class ShowRisk extends Composite {
 
     @Override
     public void resolve(List<String> historyTokens, final AsyncCallback<Widget> callback) {
-      getInstance().resolve(historyTokens, callback);
+      resolveShowRisk(historyTokens, callback);
     }
 
     @Override
@@ -66,14 +67,7 @@ public class ShowRisk extends Composite {
     }
   };
 
-  private static ShowRisk instance = null;
   private static final ClientMessages messages = GWT.create(ClientMessages.class);
-
-  interface MyUiBinder extends UiBinder<Widget, ShowRisk> {
-  }
-
-  private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
-
   private static final AsyncCallback<Actionable.ActionImpact> actionCallback = new NoAsyncCallback<Actionable.ActionImpact>() {
     @Override
     public void onSuccess(Actionable.ActionImpact result) {
@@ -82,69 +76,61 @@ public class ShowRisk extends Composite {
       }
     }
   };
+  private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+  @UiField
+  FocusPanel keyboardFocus;
+
+  @UiField
+  NavigationToolbar<IndexedRisk> navigationToolbar;
+
+  @UiField
+  TitlePanel title;
 
   @UiField(provided = true)
-  RiskShowPanel riskShowPanel;
+  BrowseRiskTabs riskTabs;
 
   @UiField
-  SimplePanel actionsSidebar;
-
-  @UiField
-  FlowPanel contentFlowPanel;
-
-  @UiField
-  FlowPanel sidebarFlowPanel;
-
-  /**
-   * Create a new panel to view a risk
-   */
-  public ShowRisk() {
-    this.riskShowPanel = new RiskShowPanel("RiskShowPanel_riskIncidences");
-    initWidget(uiBinder.createAndBindUi(this));
-  }
+  BrowseRiskActionsToolbar actionsToolbar;
 
   public ShowRisk(IndexedRisk risk) {
-    this.riskShowPanel = new RiskShowPanel(risk, "RiskShowPanel_riskIncidences");
+    riskTabs = new BrowseRiskTabs();
+    riskTabs.init(risk);
     initWidget(uiBinder.createAndBindUi(this));
-    // actionsSidebar is set in the resolve callback
+    keyboardFocus.setFocus(true);
+    keyboardFocus.addStyleName("browse browse-file browse_main_panel");
+
+    navigationToolbar.withoutButtons().build();
+    navigationToolbar.updateBreadcrumbPath(BreadcrumbUtils.getRiskBreadCrumbs(risk));
+    actionsToolbar.setLabel(messages.showRiskTitle());
+    actionsToolbar.setObjectAndBuild(risk, null, actionCallback);
+    title.setText(StringUtils.isNotBlank(risk.getName()) ? risk.getName() : risk.getId());
   }
 
-  public static ShowRisk getInstance() {
-    if (instance == null) {
-      instance = new ShowRisk();
+  private static void resolveShowRisk(List<String> historyTokens, final AsyncCallback<Widget> callback) {
+
+    if (historyTokens.size() != 1) {
+      HistoryUtils.newHistory(RiskRegister.RESOLVER);
+      callback.onSuccess(null);
+      return;
     }
-    return instance;
-  }
 
-  void resolve(List<String> historyTokens, final AsyncCallback<Widget> callback) {
+    Services services = new Services("Retrieve indexed risk", "get");
+    String riskUUID = historyTokens.get(0);
 
-    if (historyTokens.size() == 1) {
-      Services services = new Services("Retrieve indexed risk", "get");
-      SidebarUtils.showSidebar(contentFlowPanel, sidebarFlowPanel);
-
-      services
-        .rodaEntityRestService(s -> s.findByUuid(historyTokens.get(0), LocaleInfo.getCurrentLocale().getLocaleName()),
-          IndexedRisk.class)
-        .thenCompose(indexedRisk -> services.riskResource(s -> s.retrieveRiskVersions(historyTokens.get(0)))
+    services
+      .rodaEntityRestService(s -> s.findByUuid(riskUUID, LocaleInfo.getCurrentLocale().getLocaleName()),
+        IndexedRisk.class)
+      .thenCompose(indexedRisk -> services.riskResource(s -> s.retrieveRiskVersions(riskUUID))
         .whenComplete((result, throwable) -> {
           if (throwable == null) {
             indexedRisk.setHasVersions(!result.getVersions().isEmpty());
-            instance = new ShowRisk(indexedRisk);
-            RiskActions riskActions = RiskActions.get();
-            if (indexedRisk.hasVersions()) {
-              riskActions = RiskActions.getWithHistory();
-            }
-            SidebarUtils.toggleSidebar(contentFlowPanel, sidebarFlowPanel, riskActions.hasAnyRoles());
-            instance.actionsSidebar.setWidget(new ActionableWidgetBuilder<>(riskActions).withBackButton()
-              .withActionCallback(actionCallback).buildListWithObjects(new ActionableObject<>(indexedRisk)));
-            callback.onSuccess(instance);
+            callback.onSuccess(new ShowRisk(indexedRisk));
           } else {
             callback.onFailure(throwable);
           }
         }));
-    } else {
-      HistoryUtils.newHistory(RiskRegister.RESOLVER);
-      callback.onSuccess(null);
-    }
+  }
+
+  interface MyUiBinder extends UiBinder<Widget, ShowRisk> {
   }
 }
